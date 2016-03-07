33b39127f845265904407d0af02fbbbd76da7df5
wannabeCitizen <doorsofskirpan@gmail.com>
2016-03-06 22:13:02 -0700

    starting to style

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -8,8 +8,27 @@ import globalStyles from '../Toolbox/global.scss'
 export default React.createClass({
     render: function() {
         return (
-            <div className={styles.header}>
-                Global News
+            <div className={styles.main}> 
+                <div className={styles.header}>
+                    Global News
+                </div>
+                <div className={styles.tiny}>
+                    World News for Sunday March 6, 2050
+                </div>
+                <div className={styles.list}>
+                    <ul>
+                        <li>
+                            <span className={styles.special}>In The News</span>
+                        </li>
+                        <li>Finance</li>
+                        <li>Water</li>
+                        <li>War</li>
+                        <li>Health</li>
+                        <li>Refugees</li>
+                        <li>Technology</li>
+                        <li>Energy</li>
+                    </ul>
+                </div>
             </div>
         );
     }
