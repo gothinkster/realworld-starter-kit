a4ea7eb20660c9503e541fc09d8af7735b49e72b
Jacky Chiu <jacky.chiu888@gmail.com>
2017-04-26 12:47:15 -0400

    Added insertion of user in db, Update handlers to be cleaner, Added sqlite3 to gitignore

--- auth.go
+++ auth.go
@@ -4,26 +4,33 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 const createUserTable = `
-CREATE TABLE IF NOT EXISTS Users(
-	username TEXT
-	email TEXT
-	password TEXT
+CREATE TABLE IF NOT EXISTS users(
+	username TEXT,
+	email TEXT,
+	password TEXT,
+	bio TEXT
 )
 `
 
+const insertUser = `
+INSERT OR REPLACE INTO users(username, email, password)
+VALUES (?, ?, ?)
+`
+
 func CreateUserTable() {
-	db, err := sql.Open("sqlite3", "data.db")
-	defer db.Close()
+	db, err := sql.Open("sqlite3", ".sqlite3")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer db.Close()
 
 	_, err = db.Exec(createUserTable)
 	if err != nil {
@@ -39,19 +46,60 @@ type User struct {
 	Bio      string `json:"bio"`
 }
 
-type UserReqBody struct {
+func (u *User) Save(db *sql.DB) error {
+	stmt, err := db.Prepare(insertUser)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(u.Username, u.Email, u.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type UserJSON struct {
 	User `json:"user"`
 }
 
-func RegisterUser(w http.ResponseWriter, r *http.Request) {
-	user := UserReqBody{}
+func UserRouter(w http.ResponseWriter, r *http.Request) {
+	var err error
 
-	err := json.NewDecoder(r.Body).Decode(&user)
-	defer r.Body.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
+	switch r.Method {
+	case "POST":
+		err = RegisterUser(w, r)
+	default:
+		http.NotFound(w, r)
 	}
 
-	fmt.Println(user)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func RegisterUser(w http.ResponseWriter, r *http.Request) error {
+	u := UserJSON{}
+
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	db, err := sql.Open("sqlite3", ".sqlite3")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	err = u.Save(db)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprint(w, json.NewEncoder(w).Encode(u))
+	return nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	// TODO: Creation of tables in proper place
+	CreateUserTable()
+
+	http.HandleFunc("/api/users", UserRouter)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
