896f860ad6622b3da7e1cdf7b3d2a8e2e7613bdc
Omid Hezaveh <omidfi@users.noreply.github.com>
2017-05-16 08:57:15 +0300

    add error handling to server start

--- src/bin/server.js
+++ src/bin/server.js
@@ -5,8 +5,8 @@ const app = require('../app')
 process.once('SIGINT', shutDown)
 process.once('SIGTERM', shutDown) 
 
-console.log('Starting the server on: http(s)://', config.server.host, ': ', config.server.port)
-app.server.listen(config.server.port, config.server.host)
+
+app.server.listen(config.server.port, config.server.host, null, status)
 
 function shutDown () {
   console.log('Shutdown')
@@ -18,4 +18,12 @@ function shutDown () {
       app.db.destroy().catch(console.error)
     })
   }
+}
+
+// let the user know if starting the server failed
+function status(error) {
+  if(error) {
+    console.log('There was an error starting the server', error)
+  }
+  console.log('Starting the server on: ' + config.server.host + ':' + config.server.port)
 }
