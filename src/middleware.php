6be0bb616c4da13c42d2f949c868a33b3dbea8b6
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-11-08 08:13:47 -0800

    add CORS Middleware

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,12 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', $this->get('settings')['cors'])
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
--- src/settings.php
+++ src/settings.php
@@ -13,30 +13,30 @@ if (file_exists(ROOT . '.env')) {
 
 return [
     'settings' => [
-        'displayErrorDetails' => getenv('APP_DEBUG') === 'true' ? true : false, // set to false in production
+        'displayErrorDetails'    => getenv('APP_DEBUG') === 'true' ? true : false, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
         // App Settings
-        'app' => [
+        'app'                    => [
             'name' => getenv('APP_NAME'),
             'url'  => getenv('APP_URL'),
             'env'  => getenv('APP_ENV'),
         ],
 
         // Renderer settings
-        'renderer' => [
+        'renderer'               => [
             'template_path' => __DIR__ . '/../templates/',
         ],
 
         // Monolog settings
-        'logger' => [
-            'name' => getenv('APP_NAME'),
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+        'logger'                 => [
+            'name'  => getenv('APP_NAME'),
+            'path'  => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::getLevels()[mb_strtoupper(getenv('APP_LOG_LEVEL'))],
         ],
 
         // Database settings
-        'database' => [
+        'database'               => [
             'driver'    => getenv('DB_CONNECTION'),
             'host'      => getenv('DB_HOST'),
             'database'  => getenv('DB_DATABASE'),
@@ -48,12 +48,14 @@ return [
             'prefix'    => '',
         ],
 
+        'cors' => null !== getenv('CORS_ALLOWED_ORIGINS') ? getenv('CORS_ALLOWED_ORIGINS') : '*',
+
         // jwt settings
-        'jwt' => [
+        'jwt'  => [
             'secret' => getenv('JWT_SECRET'),
             'secure' => false,
             "header" => "Authorization",
             "regexp" => "/Token\s+(.*)$/i",
-        ]
+        ],
     ],
 ];
