0b9624e1b1a0b2932180e7ae54d0fc04aeca6d4d
Andrey Ovcharenko <ovcharenkoav@meta.ua>
2018-03-05 01:21:57 +0200

    remove fixtures data

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,23 +1,6 @@
 const express = require('express');
-const users = require('./users.json');
 
 const router = express.Router();
-
-router.get('/users', (req, res) => {
-  res.send(users.data.map((user, id) => ({ id, name: user[0], })));
-});
-
-router.get('/users/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.data[id];
-
-  if (!user) {
-    return res.status(404).send();
-  }
-  const [name, phone, email, birtday] = user; // eslint-disable-line comma-dangle
-
-  return res.send({ id, name, phone, email, birtday });
-});
 
 router.post('/token', (req, res) => {
   if (req.body.token) {
