59040e89c9bc86baf47e6c1b92b9bba2d07ed07a
Sina Saeedi <xesina@gmail.com>
2018-05-07 15:11:57 +0430

    initial commit, init dep and echo bootstrap

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
