59711b07758b94f3e0805961f33a74aef2f2a8ab
Sina Saeidi <xesina@gmail.com>
2018-05-24 05:28:43 +0430

    add CORS and RemoveTrailingSlash middlewares

--- router/router.go
+++ router/router.go
@@ -9,7 +9,13 @@ import (
 func New() *echo.Echo {
 	e := echo.New()
 	e.Logger.SetLevel(log.DEBUG)
+	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.Logger())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
 	e.Validator = NewValidator()
 	return e
 }
