f6adf4fc1ad3fdb0a746bf885e5bf43d436738d8
Eddie Escamilla <emartinezescamilla@gmail.com>
2019-02-09 18:05:22 -0800

    feature: add redux, redux persist, and redux dev tools

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 import React from 'react'
-import { Button, View, Text } from 'react-native';
+import { Provider } from 'react-redux'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import thunk from 'redux-thunk'
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
+
 import HomeScreen from './src/screens/HomeScreen'
 import ArticleDetailsScreen from './src/screens/ArticleDetailsScreen'
 import NewArticleScreen from './src/screens/NewArticleScreen'
 import LoginScreen from './src/screens/LoginScreen'
 import SignUpScreen from './src/screens/SignUpScreen'
 import SettingsScreen from './src/screens/SettingsScreen'
+import AppReducer from './src/redux/store'
+
+const persistentStoreBlacklist = []
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    blacklist: persistentStoreBlacklist,
+    stateReconciler: autoMergeLevel2
+}
+
+const persistedReducer = persistReducer(persistConfig, AppReducer)
+
+const getMiddleware = () => {
+    if (__DEV__) {
+        return applyMiddleware(thunk, createLogger())
+    } else {
+        // Enable additional logging in non-production environments.
+        return applyMiddleware(thunk)
+    }
+};
+
+const store = createStore(
+    persistedReducer,
+    composeWithDevTools(getMiddleware())
+)
+
+const persistor = persistStore(store)
 
 const RootStack = createStackNavigator(
     {
@@ -37,7 +74,13 @@ const RootStack = createStackNavigator(
 const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <AppContainer/>
+                </PersistGate>
+            </Provider>
+        )
+    }
 }
