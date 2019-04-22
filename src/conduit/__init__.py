15027f30341b66ce1fb93e75de070e9205eed0f0
Nejc Zupan <nejc.zupan@gmail.com>
2019-04-22 11:10:17 +0100

    expand env vars in settings

--- src/conduit/__init__.py
+++ src/conduit/__init__.py
@@ -1,12 +1,14 @@
 """Package initializer."""
 
 from pyramid.config import Configurator
+from pyramid_heroku import expandvars_dict
 
 import typing as t
 
 
 def main(global_config: t.Dict[str, str], **settings):
     """Return a Pyramid WSGI application."""
+    settings = expandvars_dict(settings)
 
     config = Configurator(settings=settings)
 
