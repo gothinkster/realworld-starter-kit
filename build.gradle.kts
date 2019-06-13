a692962204c0349895ab32dab7086c0fb80ed995
Juanjo Aguililla <juanjoaguililla@gmail.com>
2019-06-13 23:48:57 +0200

    Small improvements

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+
+plugins {
+    id("org.jetbrains.dokka") version "0.9.18" apply false
+    kotlin("jvm") version "1.3.31" apply false
+}
+
+defaultTasks("all")
+
+task("clean", type = Delete::class) {
+    delete("build", "log", "out")
+
+    delete(
+        fileTree(rootDir) { include("**/*.log") },
+        fileTree(rootDir) { include("**/*.hprof") }
+    )
+}
+
+task("all") {
+    dependsOn("backend:installDist", "backend:jarAll", "backend:doc", "frontend:assemble")
+}
