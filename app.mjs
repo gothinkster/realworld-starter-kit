d6b3ec3ae938c1341aaa6d44470fc9b9667b8c4f
tobiu <tobiasuhlig78@gmail.com>
2019-12-01 00:08:26 +0100

    removed app.mjs from the repo root

--- app.mjs
+++
@@ -1,9 +0,0 @@
-import MainContainer from './views/MainContainer.mjs';
-
-Neo.onStart = function() {
-    Neo.app({
-        appPath : 'apps/realworld/',
-        mainView: MainContainer,
-        name    : 'RealWorld'
-    });
-};
