74c42a5ab7588f4aca9dd109b94dee94db623455
jihchi <achi@987.tw>
2019-12-15 11:20:42 +0800

    Extend eslint such that CRA could accept eslintignore config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {make as App} from './App.bs';
+import { make as App } from './App.bs';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
