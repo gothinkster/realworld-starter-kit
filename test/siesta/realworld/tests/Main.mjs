e08db1f018793496bd64001466a55731dbacb0ec
tobiu <tobiasuhlig78@gmail.com>
2019-12-16 20:49:21 +0100

    siesta tests: routes

--- test/siesta/realworld/tests/Main.mjs
+++ test/siesta/realworld/tests/Main.mjs
@@ -1,7 +1,30 @@
 import Main from '../../../../node_modules/neo.mjs/src/Main.mjs';
 
 StartTest(t => {
-    t.it("Sanity", t => {
-        t.ok(Main, 'Main is imported as a JS module');
-    });
+    t.chain(
+        next => {
+            t.ok(Main, 'Main is imported as a JS module');
+            next();
+        },
+        {
+            waitFor: 'Selector', args: [ 'h1' ]
+        },
+        next => {
+            t.click('.nav-link[href="#/login"]', next);
+        },
+        next => {
+            t.is(t.query('h1')[0].innerHTML, 'Sign in');
+            next();
+        },
+        {
+            waitFor: 'Selector', args: [ 'h1' ]
+        },
+        next => {
+            t.click('.nav-link[href="#/register"]', next);
+        },
+        next => {
+            t.is(t.query('h1')[0].innerHTML, 'Sign up');
+            t.done();
+        }
+    );
 });
