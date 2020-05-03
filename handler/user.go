047478fa738eb6e88b886db06c3b43242e1ff4cb
Sina Saeidi <xesina@gmail.com>
2020-05-03 18:34:29 +0200

    added swaggo doc annotations for user handler

--- handler/user.go
+++ handler/user.go
@@ -3,11 +3,23 @@ package handler
 import (
 	"net/http"
 
+	"github.com/labstack/echo/v4"
 	"github.com/xesina/golang-echo-realworld-example-app/model"
 	"github.com/xesina/golang-echo-realworld-example-app/utils"
-	"github.com/labstack/echo/v4"
 )
 
+// SignUp godoc
+// @Summary Register a new user
+// @Description Register a new user
+// @ID sign-up
+// @Accept  json
+// @Produce  json
+// @Param user body userRegisterRequest true "User info for registration"
+// @Success 201 {object} userResponse
+// @Failure 400 {object} utils.Error
+// @Failure 404 {object} utils.Error
+// @Failure 500 {object} utils.Error
+// @Router /users [post]
 func (h *Handler) SignUp(c echo.Context) error {
 	var u model.User
 	req := &userRegisterRequest{}
@@ -20,6 +32,20 @@ func (h *Handler) SignUp(c echo.Context) error {
 	return c.JSON(http.StatusCreated, newUserResponse(&u))
 }
 
+// Login godoc
+// @Summary Login for existing user
+// @Description Login for existing user
+// @ID login
+// @Accept  json
+// @Produce  json
+// @Param user body userLoginRequest true "Credentials to use"
+// @Success 200 {object} userResponse
+// @Failure 400 {object} utils.Error
+// @Failure 401 {object} utils.Error
+// @Failure 422 {object} utils.Error
+// @Failure 404 {object} utils.Error
+// @Failure 500 {object} utils.Error
+// @Router /users/login [post]
 func (h *Handler) Login(c echo.Context) error {
 	req := &userLoginRequest{}
 	if err := req.bind(c); err != nil {
@@ -38,6 +64,20 @@ func (h *Handler) Login(c echo.Context) error {
 	return c.JSON(http.StatusOK, newUserResponse(u))
 }
 
+// CurrentUser godoc
+// @Summary Get the current user
+// @Description Gets the currently logged-in user
+// @ID current-user
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} userResponse
+// @Failure 400 {object} utils.Error
+// @Failure 401 {object} utils.Error
+// @Failure 422 {object} utils.Error
+// @Failure 404 {object} utils.Error
+// @Failure 500 {object} utils.Error
+// @Security ApiKeyAuth
+// @Router /user [get]
 func (h *Handler) CurrentUser(c echo.Context) error {
 	u, err := h.userStore.GetByID(userIDFromToken(c))
 	if err != nil {
@@ -49,6 +89,21 @@ func (h *Handler) CurrentUser(c echo.Context) error {
 	return c.JSON(http.StatusOK, newUserResponse(u))
 }
 
+// UpdateUser godoc
+// @Summary Update current user
+// @Description Update user information for current user
+// @ID update-user
+// @Accept  json
+// @Produce  json
+// @Param user body userUpdateRequest true "User details to update. At least **one** field is required."
+// @Success 200 {object} userResponse
+// @Failure 400 {object} utils.Error
+// @Failure 401 {object} utils.Error
+// @Failure 422 {object} utils.Error
+// @Failure 404 {object} utils.Error
+// @Failure 500 {object} utils.Error
+// @Security ApiKeyAuth
+// @Router /user [put]
 func (h *Handler) UpdateUser(c echo.Context) error {
 	u, err := h.userStore.GetByID(userIDFromToken(c))
 	if err != nil {
@@ -68,6 +123,21 @@ func (h *Handler) UpdateUser(c echo.Context) error {
 	return c.JSON(http.StatusOK, newUserResponse(u))
 }
 
+// GetProfile godoc
+// @Summary Get a profile
+// @Description Get a profile of a user of the system. Auth is optional
+// @ID get-profile
+// @Accept  json
+// @Produce  json
+// @Param username path string true "Username of the profile to get"
+// @Success 200 {object} userResponse
+// @Failure 400 {object} utils.Error
+// @Failure 401 {object} utils.Error
+// @Failure 422 {object} utils.Error
+// @Failure 404 {object} utils.Error
+// @Failure 500 {object} utils.Error
+// @Security ApiKeyAuth
+// @Router /profiles/{username} [get]
 func (h *Handler) GetProfile(c echo.Context) error {
 	username := c.Param("username")
 	u, err := h.userStore.GetByUsername(username)
@@ -80,6 +150,21 @@ func (h *Handler) GetProfile(c echo.Context) error {
 	return c.JSON(http.StatusOK, newProfileResponse(h.userStore, userIDFromToken(c), u))
 }
 
+// Follow godoc
+// @Summary Follow a user
+// @Description Follow a user by username
+// @ID follow
+// @Accept  json
+// @Produce  json
+// @Param username path string true "Username of the profile you want to follow"
+// @Success 200 {object} profileResponse
+// @Failure 400 {object} utils.Error
+// @Failure 401 {object} utils.Error
+// @Failure 422 {object} utils.Error
+// @Failure 404 {object} utils.Error
+// @Failure 500 {object} utils.Error
+// @Security ApiKeyAuth
+// @Router /profiles/{username}/follow [post]
 func (h *Handler) Follow(c echo.Context) error {
 	followerID := userIDFromToken(c)
 	username := c.Param("username")
@@ -95,6 +180,22 @@ func (h *Handler) Follow(c echo.Context) error {
 	}
 	return c.JSON(http.StatusOK, newProfileResponse(h.userStore, userIDFromToken(c), u))
 }
+
+// Unfollow godoc
+// @Summary Unfollow a user
+// @Description Unfollow a user by username
+// @ID unfollow
+// @Accept  json
+// @Produce  json
+// @Param username path string true "Username of the profile you want to unfollow"
+// @Success 201 {object} userResponse
+// @Failure 400 {object} utils.Error
+// @Failure 401 {object} utils.Error
+// @Failure 422 {object} utils.Error
+// @Failure 404 {object} utils.Error
+// @Failure 500 {object} utils.Error
+// @Security ApiKeyAuth
+// @Router /profiles/{username}/follow [delete]
 func (h *Handler) Unfollow(c echo.Context) error {
 	followerID := userIDFromToken(c)
 	username := c.Param("username")
@@ -110,6 +211,7 @@ func (h *Handler) Unfollow(c echo.Context) error {
 	}
 	return c.JSON(http.StatusOK, newProfileResponse(h.userStore, userIDFromToken(c), u))
 }
+
 func userIDFromToken(c echo.Context) uint {
 	id, ok := c.Get("user").(uint)
 	if !ok {
