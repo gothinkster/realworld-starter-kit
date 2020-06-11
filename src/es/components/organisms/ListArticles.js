1a7563b31c2df0e27401987f00445ec7574d1932
Silvan Struebi <silvan.struebi@gmail.com>
2020-06-11 17:45:29 +0200

    added html to home and html to article
    Signed-off-by: Silvan Struebi <silvan.struebi@gmail.com>

--- src/es/components/organisms/ListArticles.js
+++ src/es/components/organisms/ListArticles.js
@@ -6,6 +6,7 @@
 /**
  * https://github.com/Weedshaker/event-driven-web-components-realworld-example-app/blob/master/FRONTEND_INSTRUCTIONS.md#ListArticles
  * As an organism, this component shall hold molecules and/or atoms
+ * this organism always renders new when connected to keep most recent and does not need shouldComponentRender
  *
  * @export
  * @class ListArticles
@@ -45,11 +46,35 @@ export default class ListArticles extends HTMLElement {
    * @return {void}
    */
   render (multipleArticles) {
+    // TODO: Adjust if component is organism or molecule once its broken up!
+    // Could also cycle through and only replace content of existing elements, consider this when doing sub components
+    // behavior profile link
+    // format date and check if cratedAt or updatedAt
+    // behavior button favoritesCount
+    // behavior preview-link
+    // TODO: ↑↑↑
+    this.innerHTML = ''
     multipleArticles.articles.forEach(article => {
-      const el = document.createElement('div')
-      el.classList.add('article-preview')
-      el.textContent = article.title
-      this.appendChild(el)
+      const articlePreview = document.createElement('div')
+      articlePreview.classList.add('article-preview')
+      articlePreview.innerHTML = `
+        <div class="article-meta">
+          <a href="profile.html"><img src="${article.author.image}" /></a>
+          <div class="info">
+            <a href="" class="author">${article.author.username}</a>
+            <span class="date">${article.createdAt}</span>
+          </div>
+          <button class="btn btn-outline-primary btn-sm pull-xs-right">
+            <i class="ion-heart"></i> ${article.favoritesCount}
+          </button>
+        </div>
+        <a href="" class="preview-link">
+          <h1>${article.title}</h1>
+          <p>${article.description}</p>
+          <span>Read more...</span>
+        </a>
+      `
+      this.appendChild(articlePreview)
     })
   }
 }
--- src/es/components/pages/Home.js
+++ src/es/components/pages/Home.js
@@ -32,7 +32,54 @@ export default class Home extends HTMLElement {
   render () {
     this.loadChildComponents().then(() => (this.innerHTML = `
       <c-list-articles>
-        <o-list-articles></o-list-articles>
+        <div class="home-page">
+          <div class="banner">
+            <div class="container">
+              <h1 class="logo-font">conduit</h1>
+              <p>A place to share your knowledge.</p>
+            </div>
+          </div>
+
+          <div class="container page">
+            <div class="row">
+
+              <div class="col-md-9">
+                <div class="feed-toggle">
+                  <ul class="nav nav-pills outline-active">
+                    <li class="nav-item">
+                      <a class="nav-link disabled" href="">Your Feed</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" href="">Global Feed</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <o-list-articles></o-list-articles>
+
+              </div>
+
+              <div class="col-md-3">
+                <div class="sidebar">
+                  <p>Popular Tags</p>
+
+                  <div class="tag-list">
+                    <a href="" class="tag-pill tag-default">programming</a>
+                    <a href="" class="tag-pill tag-default">javascript</a>
+                    <a href="" class="tag-pill tag-default">emberjs</a>
+                    <a href="" class="tag-pill tag-default">angularjs</a>
+                    <a href="" class="tag-pill tag-default">react</a>
+                    <a href="" class="tag-pill tag-default">mean</a>
+                    <a href="" class="tag-pill tag-default">node</a>
+                    <a href="" class="tag-pill tag-default">rails</a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
       </c-list-articles>
     `))
   }
