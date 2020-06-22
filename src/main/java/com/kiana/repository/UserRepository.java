e9598772d89d1c90cc41f3f909a3df74aec515ee
Kiana Ahmadi <kiyana.ahmadi@gmail.com>
2020-06-23 01:15:51 +0430

    Add User Controller, User domain, User Repository

---
+++ src/main/java/com/kiana/repository/UserRepository.java
@@ -0,0 +1,65 @@
+package com.kiana.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class UserRepository implements CrudRepository {
+
+    @Override
+    public Object save(Object entity) {
+        return null;
+    }
+
+    @Override
+    public Iterable saveAll(Iterable entities) {
+        return null;
+    }
+
+    @Override
+    public Optional findById(Object o) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterable findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable findAllById(Iterable iterable) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Object o) {
+
+    }
+
+    @Override
+    public void delete(Object entity) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
