3e7b4e7c5b0ecfad0803ab45ceb328ccdb9d536a
carniatto <mateus.carniato@realimpactanalytics.com>
2020-08-05 16:46:17 +0300

    feat(conduit): Added ngxs, header and footer

--- apps/conduit/src/app/app.component.ts
+++ apps/conduit/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'realworld-angular-nx-ngxs-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
--- apps/conduit/src/app/app.module.ts
+++ apps/conduit/src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxsModule, NoopNgxsExecutionStrategy } from '@ngxs/store';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, RoutingModule],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    RoutingModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+      selectorOptions: {
+        injectContainerState: false,
+        suppressErrors: false,
+      },
+      executionStrategy: NoopNgxsExecutionStrategy,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
