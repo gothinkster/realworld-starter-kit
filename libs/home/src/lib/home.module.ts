f123d25a0a61142328fbef31d3d25fdd0db7b019
Joaquin Cid <joaqcid@gmail.com>
2020-08-12 09:40:54 -0300

    chore: include home lib

--- libs/home/src/lib/home.module.ts
+++ libs/home/src/lib/home.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HomePage } from './home.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-
-    RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-    ])
-  ]
+  imports: [CommonModule, RouterModule.forChild([{ path: '', pathMatch: 'full', component: HomePage }])],
+  declarations: [HomePage]
 })
 export class HomeModule {}
