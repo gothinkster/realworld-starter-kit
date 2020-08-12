b1b4fea00d0a483d53dfc2b16f53558aad48c73d
Joaquin Cid <joaqcid@gmail.com>
2020-08-12 09:39:31 -0300

    chore(home): generate home library

---
+++ libs/home/src/lib/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+
+    RouterModule.forChild([
+      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+    ])
+  ]
+})
+export class HomeModule {}
