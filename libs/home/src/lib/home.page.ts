ff50fb29f374bab970ac4b3ad3e0f5c6704d166f
Joaquin Cid <joaqcid@gmail.com>
2020-08-12 09:40:15 -0300

    feat(home): inital template for home page

---
+++ libs/home/src/lib/home.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'realworld-angular-nx-ngxs-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.css']
+})
+export class HomePage implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
