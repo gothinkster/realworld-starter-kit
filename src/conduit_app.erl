db09db1ce5d5b8d8c405e3c4b2099bf9b7690b68
Michał Łępicki <michal.lepicki@gmail.com>
2020-09-10 22:44:44 +0200

    erlang formatting

--- src/conduit_app.erl
+++ src/conduit_app.erl
@@ -1,28 +1,28 @@
 -module(conduit_app).
 
 -behaviour(application).
+
 -behaviour(supervisor).
 
--export([start/2, stop/1, init/1]).
+-export([init/1, start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-stop(_State) ->
-    ok.
+stop(_State) -> ok.
 
 init([]) ->
     SupFlags = #{strategy => one_for_all},
-    ChildSpecs = [
-        #{
-            id => conduit_elli_service,
-            start => {gleam@http@elli, start, [fun conduit:service/1, 3000]},
-            modules => [elli]
-        },
-        #{
-           id => conduit_pgo_pool,
-           start => {pgo_pool, start_link, [default, #{user => "postgres", password => "postgres", database => "conduit_dev"}]},
-           shutdown => 1000
-        }
-    ],
+    ChildSpecs = [#{id => conduit_elli_service,
+                    start =>
+                        {gleam@http@elli, start, [fun conduit:service/1, 3000]},
+                    modules => [elli]},
+                  #{id => conduit_pgo_pool,
+                    start =>
+                        {pgo_pool,
+                         start_link,
+                         [default,
+                          #{user => "postgres", password => "postgres",
+                            database => "conduit_dev"}]},
+                    shutdown => 1000}],
     {ok, {SupFlags, ChildSpecs}}.
