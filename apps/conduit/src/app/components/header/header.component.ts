fdfdf2aa8cb74f8c5e27852d2114e6527ae5b33a
Joaquin Cid <joaqcid@gmail.com>
2020-09-27 20:46:40 -0300

    feat: show/hide nav items with loggedIn status

--- apps/conduit/src/app/components/header/header.component.ts
+++ apps/conduit/src/app/components/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { AuthSelectors } from '@realworld-angular-nx-ngxs/data-access';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  loggedIn$ = this.store.select(AuthSelectors.loggedIn);
+  loggedOut$ = this.store.select(AuthSelectors.loggedOut);
+  username$ = this.store.select(AuthSelectors.username);
 
-  ngOnInit(): void {}
+  constructor(private store: Store) {}
+
+  ngOnInit() {}
 }
