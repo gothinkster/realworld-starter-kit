77463e0041f9e06a3530e5c5d13d19c806d9a89a
jihchi <achi@987.tw>
2020-10-03 19:34:14 +0800

    Upgrade react-scripts to 4.0-alpha

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { make as App } from './App.bs';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
