e424835eef26265ef0be4856b1bba631d902282e
Valetin Berger <valentin.berger@mgb.ch>
2020-12-01 16:51:28 +0100

    WIP update Login Controller

--- src/es/components/pages/Login.js
+++ src/es/components/pages/Login.js
@@ -3,6 +3,8 @@
 /* global HTMLElement */
 /* global customElements */
 
+import { Environment } from '../../helpers/Environment.js'
+
 /**
  * https://github.com/Weedshaker/event-driven-web-components-realworld-example-app/blob/master/FRONTEND_INSTRUCTIONS.md#home
  * As a page, this component becomes a domain dependent container and shall hold organisms, molecules and/or atoms
@@ -13,6 +15,38 @@
 export default class Register extends HTMLElement {
   connectedCallback () {
     if (this.shouldComponentRender()) this.render()
+    this.getElementsByTagName("form")[0].addEventListener('submit', this.submitListener);
+  }
+
+  submitListener () {
+    // @ts-ignore
+    const password = document.getElementById("password").value
+    // @ts-ignore
+    const email = document.getElementById("email").value
+    const url = `${Environment.fetchBaseUrl}users/login`
+ 
+    const body = {
+      'user': {
+        'email': email,
+        'password': password
+      }
+    }
+
+    fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }).then( (response) => {
+      return response.json()
+    }).then ( (data) => {
+      // tbd store Token and redirect
+      console.log(data);
+    })
+    
+
   }
 
   /**
@@ -38,19 +72,19 @@ export default class Register extends HTMLElement {
             <div class="col-md-6 offset-md-3 col-xs-12">
               <h1 class="text-xs-center">Sign in</h1>
               <p class="text-xs-center">
-                <a href="">Need an account?</a>
+                <a href="#/register">Need an account?</a>
               </p>
 
               <ul class="error-messages">
                 <li>That email is already taken</li>
               </ul>
 
-              <form>
+              <form id="login-form">
                 <fieldset class="form-group">
-                  <input class="form-control form-control-lg" type="text" placeholder="Email">
+                  <input class="form-control form-control-lg" id="email" type="email" placeholder="Email">
                 </fieldset>
                 <fieldset class="form-group">
-                  <input class="form-control form-control-lg" type="password" placeholder="Password">
+                  <input class="form-control form-control-lg" id="password" type="password" placeholder="Password">
                 </fieldset>
                 <button class="btn btn-lg btn-primary pull-xs-right">
                   Sign in
