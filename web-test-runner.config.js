a839bbc0af20eedf28fab6c11cc62a8bcd4e572c
jihchi <achi@987.tw>
2021-06-01 18:08:14 +0800

    Completely remove test setup

--- web-test-runner.config.js
+++
@@ -1,9 +0,0 @@
-// NODE_ENV=test - Needed by "@snowpack/web-test-runner-plugin"
-process.env.NODE_ENV = 'test';
-
-module.exports = {
-  coverageConfig: {
-    exclude: ['**/*/_snowpack/**/*'],
-  },
-  plugins: [require('@snowpack/web-test-runner-plugin')()],
-};
