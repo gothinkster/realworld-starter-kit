b5f6c1ce689b350b3d0e2cc81169cc66849bb2f0
jihchi <achi@987.tw>
2021-06-01 17:31:47 +0800

    Upgrade ReScript 9.1.3

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -2,5 +2,8 @@
 process.env.NODE_ENV = 'test';
 
 module.exports = {
+  coverageConfig: {
+    exclude: ['**/*/_snowpack/**/*'],
+  },
   plugins: [require('@snowpack/web-test-runner-plugin')()],
 };
