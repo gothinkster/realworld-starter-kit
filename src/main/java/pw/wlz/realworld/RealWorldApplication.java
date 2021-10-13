d3b4de74dc4fca022d4da7d738add64a729603af
uysong <uysong@wemb.co.kr>
2021-10-14 08:51:54 +0900

    [add] Initial Commit 환경 설정

---
+++ src/main/java/pw/wlz/realworld/RealWorldApplication.java
@@ -0,0 +1,13 @@
+package pw.wlz.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pw/wlz/realworld/RealWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.wlz.realworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
