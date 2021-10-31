90c9f2cb03a35de73f9696234cd94e70d00ffbac
Angel Martinez <angel.martineztrc@gmail.com>
2021-10-31 17:47:47 -0500

    chore:  delete testing files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,6 +5,6 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
