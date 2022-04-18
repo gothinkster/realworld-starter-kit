31f971ed45477704a348a35e93ad3b2e04ddff5a
Vinícius Vargas <vinicius.svargas@luizalabs.com>
2022-04-18 19:42:33 -0300

    chore: debug space on jwt strategy and guard

--- src/main/utils/jwt.guard.ts
+++ src/main/utils/jwt.guard.ts
@@ -1,5 +1,31 @@
-import { Injectable } from '@nestjs/common'
+import { ExecutionContext, Injectable, SetMetadata } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
+import { Reflector } from '@nestjs/core'
 
 @Injectable()
-export class JWTAuthGuard extends AuthGuard('jwt') {}
+export class JWTAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super()
+  }
+
+  canActivate(context: ExecutionContext) {
+    // Add your custom authentication logic here
+    // for example, call super.logIn(request) to establish a session.
+    console.log('Calling canActivate from JWTAuthGuard')
+    return super.canActivate(context)
+  }
+
+  handleRequest(err, user, info, context: ExecutionContext) {
+    // You can throw an exception based on either "info" or "err" arguments
+    console.log('Calling handleRequest from JWTAuthGuard')
+    return super.handleRequest(err, user, info, context)
+  }
+}
+
+export const UserIsOptional = () => SetMetadata('userIsOptional', true)
+
+export const RequireRoles = (...roles: string[]) => SetMetadata('roles', roles)
+enum Roles {
+  manageArticle = 'articles:create',
+  viewArticle = 'articles:view',
+}
