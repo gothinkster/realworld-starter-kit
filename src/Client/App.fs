0ac9062bec15374c2d4b96b9b9654072a771ff22
Riza Ramadan <riza@wartek.belajar.id>
2022-05-15 23:42:50 +0700

    feat: login page

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -8,11 +8,11 @@ open Elmish.Debug
 open Elmish.HMR
 #endif
 
-Program.mkProgram Index.init Index.update Index.view
+Program.mkProgram Index.init Index.update Index.render
 #if DEBUG
 |> Program.withConsoleTrace
 #endif
-|> Program.withReactSynchronous "elmish-app"
+|> Program.withReactSynchronous "root"
 #if DEBUG
 |> Program.withDebugger
 #endif
--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -1,91 +1,74 @@
 module Index
 
 open Elmish
-
-type Model = { Value : string }
+open Fable.React
+open Feliz
+open Feliz.Router
+type State = { CurrentUrl : string list }
 
 type Msg =
-    | ChangeValue of string
+    | UrlChanged of string list
+    | NavigateToUsers
+    | NavigateToUser of int
 
-let init () =
-    { Value = "" }, Cmd.none
+let init() = { CurrentUrl = Router.currentUrl() }, Cmd.none
 
-let update (msg:Msg) (model:Model) =
+let update msg state =
     match msg with
-    | ChangeValue newValue ->
-        { model with Value = newValue }, Cmd.none
+    | UrlChanged segments -> { state with CurrentUrl = segments }, Cmd.none
+    // notice here the use of the command Cmd.navigate
+    | NavigateToUsers -> state, Cmd.navigate("users")
+    // Router.navigate with query string parameters
+    | NavigateToUser userId -> state, Cmd.navigate("users", [ "id", userId ])
 
-open Feliz
-let view model dispatch =
+
+[<ReactComponent>]
+let layout (url: string) : ReactElement =
     Html.nav [
         prop.className "navbar navbar-light"
         prop.children [
             Html.div [
-                Html.a [
-                    prop.className "navbar-brand"
-                    prop.href "index.html"
-                    prop.text "Conduit"
-                ]
-                Html.ul [
-                    prop.className "nav navbar-nav pull-xs-right"
-                    prop.children [
-                        Html.li [
-                            prop.className "nav-item"
-                            prop.children [
-                                Html.a [
-                                    prop.className "nav-link active"
-                                    prop.href "#"
-                                    prop.text "Home"
-                                ]
-                            ]
+                prop.className "container"
+                prop.children[
+                    Html.div [
+                        Html.a [
+                            prop.className "navbar-brand"
+                            prop.href "index.html"
+                            prop.text "Conduit"
                         ]
-                        Html.li [
-                            prop.className "nav-item"
+                        Html.ul [
+                            prop.className "nav navbar-nav pull-xs-right"
                             prop.children [
-                                Html.a [
-                                    prop.className "nav-link"
-                                    prop.href "#"
+                                Html.li [
+                                    prop.className "nav-item"
                                     prop.children [
-                                        Html.i [
-                                            prop.className "ion-compose"
+                                        Html.a [
+                                            if url = ""
+                                            then prop.className "nav-link active" else prop.className "nav-link"
+                                            prop.href (Router.format(""))
+                                            prop.text "Home"
                                         ]
-                                        Html.text " New Article"
                                     ]
                                 ]
-                            ]
-                        ]
-                        Html.li [
-                            prop.className "nav-item"
-                            prop.children [
-                                Html.a [
-                                    prop.className "nav-link"
-                                    prop.href "#"
+                                Html.li [
+                                    prop.className "nav-item"
                                     prop.children [
-                                        Html.i [
-                                            prop.className "ion-gear-a"
+                                        Html.a [
+                                            if url = "login" then prop.className "nav-link active" else prop.className "nav-link"
+                                            prop.href (Router.format("login"))
+                                            prop.text "Sign In"
                                         ]
-                                        Html.text " Settings"
                                     ]
                                 ]
-                            ]
-                        ]
-                        Html.li [
-                            prop.className "nav-item"
-                            prop.children [
-                                Html.a [
-                                    prop.className "nav-link"
-                                    prop.href "#"
-                                    prop.text "Sign In"
-                                ]
-                            ]
-                        ]
-                        Html.li [
-                            prop.className "nav-item"
-                            prop.children [
-                                Html.a [
-                                    prop.className "nav-link"
-                                    prop.href "#"
-                                    prop.text "Sign Up"
+                                Html.li [
+                                    prop.className "nav-item"
+                                    prop.children [
+                                        Html.a [
+                                            if url = "register" then prop.className "nav-link active" else prop.className "nav-link"
+                                            prop.href  (Router.format("register"))
+                                            prop.text "Sign Up"
+                                        ]
+                                    ]
                                 ]
                             ]
                         ]
@@ -93,4 +76,115 @@ let view model dispatch =
                 ]
             ]
         ]
+    ]
+
+let render state dispatch =
+
+    let currentPage =
+        match state.CurrentUrl with
+        | [ ] ->
+            Html.div [
+                prop.className "container"
+                prop.children [
+                    layout ""
+                ]
+            ]
+
+        | [ "felizrouter" ] ->
+            Html.div [
+                Html.h1 "Home"
+                Html.button [
+                    prop.text "Navigate to users"
+                    prop.onClick (fun _ -> dispatch NavigateToUsers)
+                ]
+                Html.a [
+                    prop.href (Router.format("users"))
+                    prop.text "Users link"
+                ]
+                Html.a [
+                    prop.href (Router.format("login"))
+                    prop.text " Conduit Login "
+                ]
+            ]
+        | [ "users" ] ->
+            Html.div [
+                Html.h1 "Users page"
+                Html.button [
+                    prop.text "Navigate to User(10)"
+                    prop.onClick (fun _ -> dispatch (NavigateToUser 10))
+                ]
+                Html.a [
+                    prop.href (Router.format("users", ["id", 10]))
+                    prop.text "Single User link"
+                ]
+            ]
+
+        | [ "users"; Route.Query [ "id", Route.Int userId ] ] ->
+            Html.h1 (sprintf "Showing user %d" userId)
+
+        | [ "login" ] ->
+            Html.div [
+                prop.className "container"
+                prop.children [
+                    layout "login"
+                    Html.div [
+                        prop.className "row"
+                        prop.children [
+                            Html.div [
+                                prop.className "col-md-6 offset-md-3 col-xs-12"
+                                prop.children [
+                                    Html.h1 [
+                                        prop.className "text-xs-center"
+                                        prop.text "Sign in"
+                                    ]
+                                    Html.p [
+                                        prop.className "text-xs-center"
+                                        prop.children [
+                                            Html.a [
+                                                prop.href (Router.format("register"))
+                                                prop.text "Need an account?"
+                                            ]
+                                        ]
+                                    ]
+                                    Html.form[
+                                        prop.children[
+                                            Html.fieldSet [
+                                                prop.className "form-group"
+                                                prop.children [
+                                                    Html.input [
+                                                        prop.className "form-control form-control-lg"
+                                                        prop.type' "email"
+                                                        prop.placeholder "Email"
+                                                    ]
+                                                ]
+                                            ]
+                                            Html.fieldSet [
+                                                prop.className "form-group"
+                                                prop.children [
+                                                    Html.input [
+                                                        prop.className "form-control form-control-lg"
+                                                        prop.type' "password"
+                                                        prop.placeholder "Password"
+                                                    ]
+                                                ]
+                                            ]
+                                            Html.button [
+                                                prop.className "btn btn-lg btn-primary pull-xs-right"
+                                                prop.text "Sign in"
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+
+        | _ ->
+            Html.h1 "Not found"
+
+    React.router [
+        router.onUrlChanged (UrlChanged >> dispatch)
+        router.children currentPage
     ]
