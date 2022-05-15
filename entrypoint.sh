310e21ffe4c39df367ee1ad3d4ee09fe58d7f08d
ravi.leal <ravi.leal@ewally.com.br>
2022-05-15 18:30:15 -0300

    chore: add node version and docker configs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@ cd $APP_HOME
 
 if [ "$NODE_ENV" != "production" ] && [ "$DEBUG" = '0' ]; then
   echo "Start development application server..."
-  yarn knex migrate:latest && yarn dev
+  yarn knex migrate:latest ; yarn dev
 fi
 
 if [ "$NODE_ENV" != "production" ] && [ "$DEBUG" = '1' ]; then
   echo "Start debug application server..."
-  yarn knex migrate:latest && yarn debug
+  yarn knex migrate:latest ; yarn debug
 fi
 
 exec "$@"
