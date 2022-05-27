5c8b44d5fbca2df61f83798ff93028de116b6624
jaguililla <juanjoaguililla@gmail.com>
2022-05-27 13:54:37 +0200

    Update to Hexagon 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 
 plugins {
-    kotlin("jvm") version("1.6.10") apply(false)
+    kotlin("jvm") version("1.6.21") apply(false)
 }
 
 defaultTasks("build")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,4 @@
 
 rootProject.name = "real_world"
 
-gradle.rootProject {
-
-    allprojects {
-        version = "1.0"
-        group = "com.hexagonkt.realworld"
-        description = "Real World Implementation"
-    }
-}
-
 include("backend", "frontend")
