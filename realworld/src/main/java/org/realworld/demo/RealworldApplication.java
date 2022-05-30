9ed54c1e0b92fec1c0aec5a2d9651d39660caa42
hymn-fly <rlfrmsdh1@gmail.com>
2022-05-30 22:55:57 +0900

    추가 -기본 파일

---
+++ realworld/src/main/java/org/realworld/demo/RealworldApplication.java
@@ -0,0 +1,13 @@
+package org.realworld.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealworldApplication.class, args);
+	}
+
+}
---
+++ realworld/src/test/java/org/realworld/demo/RealworldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.realworld.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
