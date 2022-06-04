b1f4708f3955cf8a152282308594ac40441f8449
Vinícius Vargas <vinicius.vargas@loft.com.br>
2022-06-04 13:59:28 -0300

    refactor: view service

--- src/main/articles/views/views.service.ts
+++ src/main/articles/views/views.service.ts
@@ -14,12 +14,11 @@ export class ArticleViews {
   ) {}
 
   async getArticle(slug: string): Promise<ReadonlyArticle> {
-    const article = await this.repository.findOneBy({ slug: slug })
-    if (
-      !article ||
-      (!article.published &&
-        (!this.user || this.user.getAuthorID() !== article.authorId))
-    ) {
+    const qb = this.newQueryBuilder()
+    this.filterBySlug(qb, slug)
+    this.filterByPublishedOrOwned(qb, this.user)
+    const article = await qb.getOne()
+    if (!article) {
       throw new ArticleNotFound(slug)
     }
     return article
@@ -30,45 +29,64 @@ export class ArticleViews {
     limit: number = 20,
     offset: number = 0,
   ): Promise<ReadonlyArticle[]> {
-    const qb = this.repository
-      .createQueryBuilder()
-      .limit(limit)
-      .skip(offset)
-      .where(
-        new Brackets((qb) => {
-          qb.where({ published: true })
-          if (this.user) {
-            qb.orWhere({ authorId: this.user.getAuthorID() })
-          }
-        }),
-      )
+    const qb = this.newQueryBuilder(limit, offset)
+    this.filterByPublishedOrOwned(qb, this.user)
+    this.filterByTags(qb, filters.tags?.split(','))
+    await this.filterByAuthor(qb, filters.author)
 
-    if (filters.author) {
-      const author = await this.profiles.getProfile({
-        username: filters.author,
-      })
-      if (!author) {
-        return []
-      } else {
-        qb.andWhere({ authorId: author.getAuthorID() })
-      }
-    }
-    await ArticleViews.filterByTags(qb, filters.tags)
-    const query = qb.getQuery()
-    const result = await qb.getMany()
-    return result
+    return await qb.getMany()
   }
 
-  private static async filterByTags(
+  private newQueryBuilder(
+    limit: number = 20,
+    offset: number = 0,
+  ): SelectQueryBuilder<ArticleEntity> {
+    const qb = this.repository
+      .createQueryBuilder()
+      .where('true')
+      .limit(limit)
+      .skip(offset)
+    qb.leftJoinAndSelect(`${qb.alias}.tagList`, 'tags')
+    return qb
+  }
+
+  private filterBySlug(qb: SelectQueryBuilder<ArticleEntity>, slug: string) {
+    qb.andWhere({ slug: slug })
+  }
+
+  private async filterByAuthor(
     qb: SelectQueryBuilder<ArticleEntity>,
-    tags: string | null | undefined,
+    author?: string,
   ) {
-    if (!tags) {
-      qb.leftJoinAndSelect(`${qb.alias}.tagList`, 'tags')
-    } else {
-      qb.leftJoin('ArticlesHaveTags', 'aht', `aht.articleId = ${qb.alias}.id`)
-      qb.leftJoin('TagEntity', 'tags', 'tags.id = aht.tagId')
-      qb.andWhere('tags.name IN (:...tags)', { tags: tags.split(',') })
+    if (author) {
+      const authorProfile = await this.profiles.getProfile({
+        username: author,
+      })
+      if (!authorProfile) {
+        qb.andWhere('false')
+      } else {
+        qb.andWhere({ authorId: authorProfile.getAuthorID() })
+      }
     }
+  }
+
+  private filterByTags(qb: SelectQueryBuilder<ArticleEntity>, tags: string[]) {
+    if (tags && tags !== []) {
+      qb.andWhere('tags.name IN (:...tags)', { tags: tags })
+    }
+  }
+
+  private filterByPublishedOrOwned(
+    qb: SelectQueryBuilder<ArticleEntity>,
+    author?: Author,
+  ) {
+    qb.andWhere(
+      new Brackets((qb) => {
+        qb.where({ published: true })
+        if (author) {
+          qb.orWhere({ authorId: author.getAuthorID() })
+        }
+      }),
+    )
   }
 }
