b6906e8f1cb4bb28d008964f6cbdd1bb0d3beb03
Shai Falach <shaifalach18@gmail.com>
2022-07-31 08:12:39 +0300

    new branch to test error

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,45 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import solid from "solid-start";
-import devtoolsPlugin from "solid-devtools/vite";
+import { defineConfig } from 'vite'
+import solid from 'solid-start'
 
-import type { UserConfig } from "vitest/config";
+import type { UserConfig } from 'vitest/config'
 
 const vitestConfig: UserConfig = {
-  test: {
-    globals: true,
-    environment: "jsdom",
-    transformMode: {
-      web: [/\.[tj]sx?$/],
-    },
-    setupFiles: "./scripts/setup-vitest.ts",
-    // solid needs to be inline to work around
-    // a resolution issue in vitest:
-    deps: {
-      inline: [/solid-js/],
-    },
-    // if you have few tests, try commenting one
-    // or both out to improve performance:
-    // threads: false,
-    // isolate: false,
-  },
-};
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		transformMode: {
+			web: [/\.[tj]sx?$/]
+		},
+		setupFiles: './scripts/setup-vitest.ts',
+		// solid needs to be inline to work around
+		// a resolution issue in vitest:
+		deps: {
+			inline: [/solid-js/]
+		}
+		// if you have few tests, try commenting one
+		// or both out to improve performance:
+		// threads: false,
+		// isolate: false,
+	}
+}
 
 const viteConfig = defineConfig({
-  test: vitestConfig.test,
-  plugins: [
-    devtoolsPlugin(),
-    solid({
-      ssr: true,
-    }),
-  ],
-  build: {
-    target: "esnext",
-    polyfillModulePreload: false,
-    // polyfillDynamicImport: false
-  },
-  resolve: {
-    conditions: ["development", "browser"],
-  },
-}) as UserConfig;
+	test: vitestConfig.test,
+	plugins: [
+		solid({
+			ssr: true
+		})
+	],
+	build: {
+		target: 'esnext',
+		polyfillModulePreload: false
+		// polyfillDynamicImport: false
+	},
+	resolve: {
+		conditions: ['development', 'browser']
+	}
+}) as UserConfig
 
-export default viteConfig;
+export default viteConfig
