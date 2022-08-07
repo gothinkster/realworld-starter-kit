dc5a9b727b4f18ed328bb0928dc1cb2af9e3ae1b
Riza Ramadan <riza.ramadan@gmail.com>
2022-08-07 14:17:19 +0700

    error: still cannot compile

--- src/Client/Components/PageLogin.fs
+++ src/Client/Components/PageLogin.fs
@@ -3,72 +3,102 @@
 open Feliz
 open Feliz.Router
 
-let loginForm =
-    Html.div [
-        prop.className "home-page"
-        prop.children [
-            Html.div [
-                prop.className "row"
-                prop.children [
-                    Html.div [
-                        prop.className "col-md-6 offset-md-3 col-xs-12"
-                        prop.children [
-                            Html.h1 [
-                                prop.className "text-xs-center"
-                                prop.text "Sign in"
-                            ]
-                            Html.p [
-                                prop.className "text-xs-center"
-                                prop.children [
-                                    Html.a [
-                                        prop.href (Router.format("register"))
-                                        prop.text "Need an account?"
-                                    ]
-                                ]
-                            ]
-                            Html.form[
-                                prop.children[
-                                    Html.fieldSet [
-                                        prop.className "form-group"
-                                        prop.children [
-                                            Html.input [
-                                                prop.className "form-control form-control-lg"
-                                                prop.type' "email"
-                                                prop.placeholder "Email"
+type State =
+    { Username: string
+      Password: string
+      LoginAttempt: string }
+
+type Msg =
+    | UsernameChanged of string
+    | PasswordChanged of string
+    | Login
+
+open Elmish
+let init() =
+    { Username = ""
+      Password = ""
+      LoginAttempt = "" }, Cmd.none
+
+
+let update (msg: Msg) (state: State) =
+    match msg with
+    | UsernameChanged username ->
+        { state with Username = username  }, Cmd.none
+
+    | PasswordChanged password ->
+        { state with Password = password }, Cmd.none
+
+    | Login ->
+        let nextState = { state with LoginAttempt = "success" }
+        let login = async {
+            return "token-abc"
+        }
+        nextState, Cmd.none
+
+open Components.LayoutGuess
+[<ReactComponent>]
+let PageLogin (dispatch: Msg -> Unit) : ReactElement =
+        Html.div [
+            prop.className "home-page"
+            prop.children [
+                LayoutGuess "login"
+                Html.div [
+                    prop.className "home-page"
+                    prop.children [
+                        Html.div [
+                            prop.className "row"
+                            prop.children [
+                                Html.div [
+                                    prop.className "col-md-6 offset-md-3 col-xs-12"
+                                    prop.children [
+                                        Html.h1 [
+                                            prop.className "text-xs-center"
+                                            prop.text "Sign in"
+                                        ]
+                                        Html.p [
+                                            prop.className "text-xs-center"
+                                            prop.children [
+                                                Html.a [
+                                                    prop.href (Router.format("register"))
+                                                    prop.text "Need an account?"
+                                                ]
                                             ]
                                         ]
-                                    ]
-                                    Html.fieldSet [
-                                        prop.className "form-group"
-                                        prop.children [
-                                            Html.input [
-                                                prop.className "form-control form-control-lg"
-                                                prop.type' "password"
-                                                prop.placeholder "Password"
+                                        Html.form[
+                                            prop.children[
+                                                Html.fieldSet [
+                                                    prop.className "form-group"
+                                                    prop.children [
+                                                        Html.input [
+                                                            prop.className "form-control form-control-lg"
+                                                            prop.type' "email"
+                                                            prop.placeholder "Email"
+                                                        ]
+                                                    ]
+                                                ]
+                                                Html.fieldSet [
+                                                    prop.className "form-group"
+                                                    prop.children [
+                                                        Html.input [
+                                                            prop.className "form-control form-control-lg"
+                                                            prop.type' "password"
+                                                            prop.placeholder "Password"
+                                                        ]
+                                                    ]
+                                                ]
+                                                Html.button [
+                                                    prop.className "btn btn-lg btn-primary pull-xs-right"
+                                                    prop.onClick (fun _ -> dispatch Login)
+                                                    prop.text "Sign in"
+                                                ]
                                             ]
                                         ]
-                                    ]
-                                    Html.button [
-                                        prop.className "btn btn-lg btn-primary pull-xs-right"
-                                        prop.text "Sign in"
                                     ]
                                 ]
                             ]
                         ]
                     ]
                 ]
-            ]
-        ]
-    ]
-
-open Components.LayoutGuess
-[<ReactComponent>]
-let PageLogin : ReactElement =
-        Html.div [
-            prop.className "home-page"
-            prop.children [
-                LayoutGuess "login"
-                loginForm
             ]
         ]
 
--- src/Client/Conduit.fs
+++ src/Client/Conduit.fs
@@ -18,29 +18,34 @@ type State = {
     UserState : UserState
 }
 
-type Msg = UrlChanged of string list
+type Message =
+    | UrlChanged of string list
+    | Login of PageLogin.Msg
+
 
 let init() = { CurrentUrl = Router.currentUrl(); UserState = UserState.Guess }, Cmd.none
 
 let update msg state =
     match msg with
     | UrlChanged segments -> { state with CurrentUrl = segments }, Cmd.none
+    | Login msg -> { state with UserState = UserState.LoggedIn }, Cmd.none
 
 open Components.PageLogin
 open Components.PageRegister
-let render state dispatch =
+let render state (dispatch: Message -> Unit) =
     let currentPage =
         match state.UserState with
         | Guess ->
             match state.CurrentUrl with
             | [ ]            -> PageGuestHome
-            | [ "login" ]    -> PageLogin
+            | [ "login" ]    -> PageLogin Message.Login
             | [ "register" ] -> PageRegister
-            | _ ->
+            | _              ->
                 Html.h1 "Not found"
         | LoggedIn ->
             match state.CurrentUrl with
-            | _ ->
+            | [] -> PageGuestHome
+            | _  ->
                 Html.h1 "Not found"
 
     React.router [
