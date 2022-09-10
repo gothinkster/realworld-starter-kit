2568951c1e04a21d48203afd0991b64fe30e6040
Ran Wahle <ranwahle@Ran-Wahle-MacBook-Pro-P79N7YFNWR.local>
2022-09-10 15:28:22 +0300

    get tags before SSR

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,32 +1,22 @@
 import { useStore, component$, useWatch$, useClientEffect$ } from '@builder.io/qwik';
 import { Header } from '../header/header';
 import { Logo } from '../logo/logo';
-
+import axios from 'axios';
 export const buttonClicked =(...args: any[]) => {
   console.log('button clicked', ...args);
 }
 
-export const getTags: () => Promise<any[]> = async () => {
-  const response = await fetch('https://api.realworld.io/api/tags');
-  try {
-    const data = await response.json() as {tags: any[]};
-    console.log('tags', data);
-    return data.tags;
-  } catch(err) {
-    if (!response.ok) {
-      console.error(response.statusText)
-    }
-    return [];
-  }
-
+export const getTags = async () => {
+  const response = await axios.get('https://api.realworld.io/api/tags');
+  return response.data.tags;
 }
 
+export const App = component$(async () => {
 
-export const App = component$(() => {
   const state = useStore({ tags:['tag'] });
-  useClientEffect$(async () => {
-   state.tags = await getTags();
-  })  
+
+  const tags = await getTags();
+  state.tags = tags;
   return (
     <div class="my-app p-20">
       <Header></Header>
