831a005b70bd056fa6c6eccc31d1a15481273693
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2022-09-26 20:31:54 -0300

    feat(routes): add some routes

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,4 @@
 import { component$, Slot } from "@builder.io/qwik";
-import Header from "../components/header/header";
 
 export default component$(() => {
   return (
@@ -7,32 +6,32 @@ export default component$(() => {
       <main>
         <nav class="navbar navbar-light">
           <div class="container">
-            <a class="navbar-brand" href="index.html">
+            <a class="navbar-brand" href="/">
               conduit
             </a>
             <ul class="nav navbar-nav pull-xs-right">
               <li class="nav-item">
-                <a class="nav-link active" href="">
+                <a class="nav-link active" href="/">
                   Home
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="">
+                <a class="nav-link" href="/editor">
                   <i class="ion-compose"></i>&nbsp;New Article
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="">
+                <a class="nav-link" href="/settings">
                   <i class="ion-gear-a"></i>&nbsp;Settings
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="">
+                <a class="nav-link" href="/login">
                   Sign in
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="">
+                <a class="nav-link" href="/register">
                   Sign up
                 </a>
               </li>
