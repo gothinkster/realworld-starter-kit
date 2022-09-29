cc90bd7cda7a487571762d037a6723c3c4928601
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2022-09-29 10:39:00 -0300

    chore(terraform): add terraform folder to use IaaS with Cloudflare

---
+++ terraform/cloudflare.tf
@@ -0,0 +1,66 @@
+terraform {
+  cloud {
+    organization = "gangoffront"
+
+    workspaces {
+      name = "gangoffront-realworld-qwik"
+    }
+  }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "cloudflare" {}
+
+variable "zone_id" {
+  default = "0b140084d6f4cd3e6b278eedcf6fec1a"
+}
+
+variable "account_id" {
+  default = "5df477d4f9a8cf72185ef8f44fd1e144"
+}
+
+variable "domain" {
+  default = "gangoffront.com"
+}
+
+variable "project_name" {
+  default = "gangoffront-com"
+}
+
+resource "cloudflare_record" "realworld_qwik" {
+  name    = "realworld-qwik"
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = "gangoffront-realworld-qwik.pages.dev"
+  zone_id = var.zone_id
+}
+
+resource "cloudflare_pages_project" "gangoffront_realworld_qwik" {
+  account_id        = var.account_id
+  name              = "gangoffront-realworld-qwik"
+  production_branch = "master"
+  build_config {
+    destination_dir = "dist"
+    build_command   = "npm run build"
+    root_dir        = "/"
+  }
+  source {
+    type = "github"
+    config {
+      owner                         = "gang-of-front"
+      repo_name                     = "realworld-qwik"
+      production_branch             = "master"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_branch_includes       = ["preview"]
+      preview_branch_excludes       = ["master"]
+    }
+  }
+}
