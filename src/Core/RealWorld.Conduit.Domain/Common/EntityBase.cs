bf998c962c8655f0e27cc09387fd6951cfb63a94
Edward Jr <45992962+edward-teixeira@users.noreply.github.com>
2022-10-22 21:20:12 -0300

    Lint

--- src/Core/RealWorld.Conduit.Domain/Common/EntityBase.cs
+++ src/Core/RealWorld.Conduit.Domain/Common/EntityBase.cs
@@ -2,12 +2,12 @@
 {
     public abstract class EntityBase : IEntity<Guid>
     {
-        public Guid Id { get; }
-
         protected EntityBase()
         {
             Id = Guid.NewGuid();
         }
+
+        public Guid Id { get; }
     }
 
     public abstract class EntityBase<TId> : IEntity<TId>
