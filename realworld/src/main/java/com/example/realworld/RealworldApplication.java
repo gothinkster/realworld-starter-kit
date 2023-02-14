1c934796bd92531144c63ccccb6ec10f5835010f
llldagolll <72905484+llldagolll@users.noreply.github.com>
2023-02-14 16:44:25 +0900

    home.htmlを返すように

--- realworld/src/main/java/com/example/realworld/RealworldApplication.java
+++ realworld/src/main/java/com/example/realworld/RealworldApplication.java
@@ -3,6 +3,7 @@ package com.example.realworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,14 +15,9 @@ public class RealworldApplication {
     }
 
     @GetMapping("/hello")
-    public String getHello() {
-        return "hello";
-    }
+    public String getHello(@RequestParam(value = "name", defaultValue = "World") String name) {
 
-
-    @GetMapping("/hello/world")
-    public String getWorld() {
-        return "world";
+        return String.format("Hello %s!", name);
     }
 
 }
