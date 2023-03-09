090652264fa280ed5459db7135d76478d6b6ea7f
Chau Than <simplesoul.chau@gmail.com>
2023-03-09 13:21:50 +0700

    Add EF Core

---
+++ Conduit/src/Conduit.API/Infrastructure/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Conduit.API.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
--- Conduit/src/Conduit.API/Program.cs
+++ Conduit/src/Conduit.API/Program.cs
@@ -1,4 +1,15 @@
+using Conduit.API.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+services.AddDbContext<AppDbContext>(
+    opt => opt.UseSqlServer(
+        configuration.GetConnectionString("ConnectionString")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
