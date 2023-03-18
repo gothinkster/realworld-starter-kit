2cbd7d8bad0289e19156a0f7511bc7c2065a1096
aliml92 <almowins5yearsss@gmail.com>
2023-03-18 11:56:41 +0900

    cleanup

--- api/server.go
+++ api/server.go
@@ -1,9 +1,30 @@
 package api
 
-import "github.com/gorilla/mux"
+import (
+	db "github.com/aliml92/realworld-go-sqlc/db/sqlc"
+	"github.com/gorilla/mux"
+)
 
 
 
 type Server struct {
-	mux.Router
+	router *mux.Router
+	store db.Querier
+}
+
+
+func NewServer(store db.Querier) *Server {
+	server := &Server{store: store}
+	router := mux.NewRouter()
+	server.router = router
+	return server
+}
+
+
+func (s *Server) MountHandlers() {
+	
+}
+
+func (s *Server) Start(addr string) error {
+	return nil
 }
