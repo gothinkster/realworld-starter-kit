7de1aacc2965cc267be56c2c33c0a34193f46770
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-03-26 20:08:12 -0300

    chore: decrease timeout to 10 seconds

--- scripts/wait_for_status.sh
+++ scripts/wait_for_status.sh
@@ -1,3 +1,3 @@
 STATUS=$1
 echo "Waiting url" "$URL" "for status code" "$STATUS"
-timeout 30s sh -c "until [ $(curl -s -o /dev/null -w "%{http_code}" "$URL") = $STATUS ]; do sleep 1; done; echo 'Server is up and running'" || (echo "Timeout reached" && exit 1)
+timeout 10s sh -c "until [ $(curl -s -o /dev/null -w "%{http_code}" "$URL") = $STATUS ]; do sleep 1; done; echo 'Server is up and running'" || (echo "Timeout reached" && exit 1)
