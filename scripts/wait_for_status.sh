cadc5caba19b190852f015d5bd3f9f7dca984302
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-03-26 21:49:29 -0300

    chore: reduce script lines

--- scripts/wait_for_status.sh
+++ scripts/wait_for_status.sh
@@ -1,30 +1,6 @@
-#! /bin/bash
-
+#! /bin/sh
+URL=$1
+STATUS=$2
 TIMEOUT_SECONDS=30
-URL="$1"
-STATUS_CODE="$2"
-
-if [[ -z "$URL" || -z "$STATUS_CODE" ]]; then
-  echo "Usage: $0 URL STATUS_CODE"
-  exit 1
-fi
-
-if ! [[ "$STATUS_CODE" =~ ^[0-9]+$ ]]; then
-  echo "STATUS_CODE must be a number"
-  exit 1
-fi
-
-echo "Waiting for $URL to return HTTP $STATUS_CODE"
-
-if ! timeout $TIMEOUT_SECONDS sh -c "until curl --silent -v --head --fail $URL >/dev/null; do sleep 1; done"; then
-  echo "Timed out"
-  exit 1
-fi
-
-if curl --silent --head --fail "$URL" | head -n 1 | grep -q "$STATUS_CODE"; then
-  echo "Server started successfully"
-  exit 0
-else
-  echo "Server did not return HTTP $STATUS_CODE"
-  exit 1
-fi
+echo "Waiting url" "$URL" "to return status code" "$STATUS"
+timeout $TIMEOUT_SECONDS sh -c "until [ $(curl -s -o /dev/null -w "%{http_code}" "$URL") = $STATUS ]; do sleep 1; done; echo 'Server is up and running'" || (echo "Timeout reached" && exit 1)
