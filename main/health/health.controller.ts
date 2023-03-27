6db93f73223de2e2d541c15df7ce6f45a7fb2e00
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-03-27 09:41:09 -0300

    fix: readiness checks

--- main/health/health.controller.ts
+++ main/health/health.controller.ts
@@ -1,23 +1,13 @@
 import { Controller, Get, Res } from '@nestjs/common'
-import { Axios } from 'axios'
-import { API_PORT, GLOBAL_PREFIX } from '../global/constants'
+import { ArticlesService } from '../articles/articles.service'
 
-@Controller('health')
+@Controller('checks')
 export class HealthController {
-  private axios = new Axios({
-    baseURL: `http://localhost:${API_PORT}/${GLOBAL_PREFIX}`,
-    responseType: 'json',
-    transformRequest: (data) => (data ? JSON.stringify(data) : data),
-    transformResponse: (data) => (data ? JSON.parse(data) : data),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    validateStatus: (status) => status < 500,
-  })
+  constructor(private readonly articlesService: ArticlesService) {}
 
-  @Get()
+  @Get('readiness')
   async healthCheck(@Res() res) {
-    const articlesResponse = await this.axios.get('articles')
-    return res.status(articlesResponse.status)
+    await this.articlesService.getView().getArticlesByFilters({})
+    return res.status(200)
   }
 }
--- main/health/health.module.ts
+++ main/health/health.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { HealthController } from './health.controller'
+import { ArticlesModule } from '../articles/articles.module'
 
 @Module({
   controllers: [HealthController],
+  imports: [ArticlesModule],
 })
 export class HealthModule {}
