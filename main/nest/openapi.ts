77530ce3ff51eabc546e93ea1e6e03bbd77febdf
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-03-28 20:26:01 -0300

    chore: script to generate openapi

--- main/nest/openapi.ts
+++ main/nest/openapi.ts
@@ -1,10 +1,14 @@
 import { INestApplication } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { OpenAPIObject } from '@nestjs/swagger/dist/interfaces'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from '../app.module'
+import { GLOBAL_PREFIX } from '../global/constants'
+import * as fs from 'fs'
 
 export function createOpenAPI(app: INestApplication): OpenAPIObject {
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('Magazine API')
+    .setTitle('Real World API')
     .setDescription(
       'A Rest API for publishing articles and consuming articles from authors you follow.',
     )
@@ -13,4 +17,14 @@ export function createOpenAPI(app: INestApplication): OpenAPIObject {
     .addBearerAuth()
     .build()
   return SwaggerModule.createDocument(app, swaggerConfig)
+}
+
+if (require.main === module) {
+  NestFactory.create(AppModule).then((app) => {
+    app.setGlobalPrefix(GLOBAL_PREFIX)
+    fs.writeFileSync(
+      'openapi.json',
+      JSON.stringify(createOpenAPI(app), null, 2),
+    )
+  })
 }
