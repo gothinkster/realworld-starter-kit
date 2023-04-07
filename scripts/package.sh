7f2a43114a564d1f0e6d6bfc445b5b87f0cfb71d
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-04-07 15:51:02 -0300

    chore: deps

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 # Clean
-rm -rf dist
+rm -rf dist build build.zip
 
 # Build
 npm run build
 npm ci --omit dev
-rm -rf dist/acceptance && mv dist/main/* dist/ && rm -rf dist/main
-cp -r node_modules dist/node_modules
+mv dist/main build
+rm -rf dist
+cp -r node_modules build/node_modules
+zip -r build.zip build
--- scripts/ziplambda.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-# Clean
-./scripts/package.sh
-zip -r dist.zip dist
-mv dist.zip terraform/lambda/dist.zip
