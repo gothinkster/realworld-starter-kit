83eb1808a43b2117341d227f9d911ee15836a717
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-04-08 17:02:47 -0300

    chore: change function_url to api gateway

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -29,6 +29,7 @@ locals {
     Environment = var.ENVIRONMENT
     RepoLink    = "https://github.com/santunioni/realworld-app"
   }
+  name = "realworld-api-${var.ENVIRONMENT}"
 }
 
 
@@ -36,7 +37,7 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "realworld_api_function_role" {
-  name               = "realworld-api-function-${var.ENVIRONMENT}"
+  name               = local.name
   assume_role_policy = <<EOF
     {
       "Version": "2012-10-17",
@@ -60,7 +61,7 @@ EOF
 }
 
 resource "aws_lambda_function" "realworld_api_function" {
-  function_name = "realworld-api-function-${var.ENVIRONMENT}"
+  function_name = local.name
   role          = aws_iam_role.realworld_api_function_role.arn
   tags          = local.COMMON_TAGS
   provider      = aws
@@ -75,12 +76,130 @@ resource "aws_lambda_function" "realworld_api_function" {
   runtime  = "nodejs16.x"
 }
 
-resource "aws_lambda_function_url" "realworld_api_function_url" {
-  function_name      = aws_lambda_function.realworld_api_function.arn
-  authorization_type = "NONE"
-  provider           = aws
+# API Gateway
+resource "aws_api_gateway_rest_api" "api" {
+  name                     = local.name
+  description              = "API for ${local.name}"
+  binary_media_types       = ["*/*"]
+  minimum_compression_size = 0
 }
 
-output "API_URL" {
-  value = aws_lambda_function_url.realworld_api_function_url.function_url
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+# Requests mapping
+resource "aws_api_gateway_method" "request_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "request_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_method.request_method.resource_id
+  http_method = aws_api_gateway_method.request_method.http_method
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.realworld_api_function.invoke_arn
+
+  # AWS lambdas can only be invoked with the POST method
+  integration_http_method = "POST"
+}
+
+# OPTIONS are needed for COORS
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"      = true
+    "method.response.header.Access-Control-Allow-Headers"     = true
+    "method.response.header.Access-Control-Allow-Methods"     = true
+    "method.response.header.Access-Control-Allow-Credentials" = true
+  }
+
+  depends_on = [aws_api_gateway_method.options_method]
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.options_method.http_method
+
+  type             = "MOCK"
+  content_handling = "CONVERT_TO_TEXT"
+
+  depends_on = [aws_api_gateway_method.options_method]
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.options_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,DELETE,GET,HEAD,PATCH,POST,PUT'"
+  }
+
+  depends_on = [
+    aws_api_gateway_method_response.options_200,
+    aws_api_gateway_integration.options_integration,
+  ]
+}
+
+# Deployment
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = var.ENVIRONMENT
+
+  depends_on = [
+    aws_api_gateway_integration.options_integration,
+    aws_api_gateway_integration.request_integration,
+  ]
+}
+
+# API Permissions
+resource "aws_lambda_permission" "allow_api_gateway" {
+  function_name = aws_lambda_function.realworld_api_function.arn
+  statement_id  = "AllowExecutionFromApiGateway"
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+
+  depends_on = [
+    aws_api_gateway_rest_api.api,
+    aws_api_gateway_resource.proxy,
+  ]
+}
+
+output "http_method" {
+  value = aws_api_gateway_method.request_method.http_method
+}
+
+output "url" {
+  value = aws_api_gateway_deployment.deployment.invoke_url
+}
+
+output "rest_api_id" {
+  value = aws_api_gateway_rest_api.api.id
 }
