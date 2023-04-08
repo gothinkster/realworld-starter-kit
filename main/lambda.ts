a25e3f4f2038d305fa29082f2687c44948653011
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-04-08 17:23:52 -0300

    chore: server initialization

--- main/lambda.ts
+++ main/lambda.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core'
-import { ExpressAdapter } from '@nestjs/platform-express'
 import { SwaggerModule } from '@nestjs/swagger'
 import { Context } from 'aws-lambda'
 import * as serverlessExpress from 'aws-serverless-express'
-import * as express from 'express'
 import { Server } from 'http'
 import { AppModule } from './app.module'
 import { GLOBAL_PREFIX } from './global/constants'
@@ -13,17 +11,15 @@ let lambdaProxyServer: Server
 
 export async function handler(event: any, context: Context) {
   if (!lambdaProxyServer) {
-    const expressServer = express()
-    const app = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressServer),
-    )
+    const app = await NestFactory.create(AppModule)
     app.setGlobalPrefix(GLOBAL_PREFIX)
     SwaggerModule.setup('docs', app, createOpenAPI(app), {
       useGlobalPrefix: true,
     })
     await app.init()
-    lambdaProxyServer = serverlessExpress.createServer(expressServer)
+    lambdaProxyServer = serverlessExpress.createServer(
+      app.getHttpServer().getInstance(),
+    )
   }
   console.log({ event, context })
   serverlessExpress.proxy(lambdaProxyServer, event, context)
