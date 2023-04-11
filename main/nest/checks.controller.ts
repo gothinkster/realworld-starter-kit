987565f63fdb919be994fbfaea3ac544f4116944
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-04-10 22:09:12 -0300

    fix: readiness check

--- main/nest/checks.controller.ts
+++ main/nest/checks.controller.ts
@@ -1,35 +1,22 @@
-import { Controller, Get, HttpCode, HttpStatus, Res } from '@nestjs/common'
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
-import axios, { AxiosError } from 'axios'
-import { Response } from 'express'
-import * as path from 'path'
+import { EntityManager } from 'typeorm'
 import { getEnvs } from '../environment'
 
 @ApiTags('checks')
 @Controller('checks')
 export class ChecksController {
-  @Get('readiness')
-  async getReadiness(@Res() res: Response) {
-    try {
-      const { API_PREFIX, API_PORT } = getEnvs()
-      const url = `http://localhost:${path.join(
-        String(API_PORT),
-        API_PREFIX,
-        'articles',
-      )}`
-      const articles = await axios.get(url)
-      res.status(articles.status).send(articles.status === 200)
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        res.status(error.response?.status || 500).send(false)
-      }
-    }
-  }
+  constructor(private readonly entityManager: EntityManager) {}
 
-  @Get('version')
+  @Get('readiness')
   @HttpCode(HttpStatus.OK)
-  getVersion() {
+  async getReadiness() {
     const { VERSION } = getEnvs()
-    return VERSION
+    const res = await this.entityManager.query('SELECT 1')
+    const ready = res.length === 1 && res[0]['1'] === '1'
+    return {
+      version: VERSION,
+      ready,
+    }
   }
 }
