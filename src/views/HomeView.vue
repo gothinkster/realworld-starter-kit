525a93fa12bc8c46d873604cdb9b86d849929d29
Carl West <frankiewb9527@gmail.com>
2023-06-27 09:49:14 +0800

    style: delete default file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [ vue() ],
+  server: { host: true, open: true },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
