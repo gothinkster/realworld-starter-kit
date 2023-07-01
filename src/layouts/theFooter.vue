fc3723ff3057660aa379ba843a170d3c5798c0e7
Carl West <frankiewb9527@gmail.com>
2023-07-01 21:35:34 +0800

    feat: add the footer

---
+++ src/layouts/theFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>
+    <div class="container">
+      <a href="/" class="logo-font" @click="$router.push('/')">conduit</a>
+      <span class="attribution">
+        An interactive learning project from
+        <a href="https://thinkster.io" target="_blank">Thinkster</a>. Code &amp; design licensed
+        under MIT.
+      </span>
+    </div>
+  </footer>
+</template>
