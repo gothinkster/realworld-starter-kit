4498c52cf903a1cb89f7988c8680d41a63497874
Carl West <frankiewb9527@gmail.com>
2023-07-01 21:36:37 +0800

    feat: add lyaouts

---
+++ src/layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import theFooter from './theFooter.vue'
+import theHeader from './theHeader.vue'
+</script>
+
+<template>
+  <the-header />
+  <router-view />
+  <the-footer />
+</template>
