42cf921f2497c5098b578dc84b2527ed02bc799e
Carl West <frankiewb9527@gmail.com>
2023-07-01 21:05:09 +0800

    feat: add the header

---
+++ src/layouts/theHeader.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+interface NavList {
+  label: string
+  path: string
+}
+
+const navList = reactive<NavList[]>([
+  {
+    label: 'Home',
+    path: '/'
+  },
+  {
+    label: 'Sign in',
+    path: '/login'
+  },
+  {
+    label: 'Sign up',
+    path: '/register'
+  }
+])
+</script>
+
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <a class="navbar-brand" @click="$router.push('/')">conduit</a>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item" :key="index" v-for="({ label, path }, index) in navList">
+          <a
+            :class="['nav-link', { active: $router.currentRoute.value.fullPath === path }]"
+            @click="$router.push(path)">
+            {{ label }}
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
