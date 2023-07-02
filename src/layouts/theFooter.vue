a82297f91f95c42cca7f36f2e8e7276444814040
Carl West <frankiewb9527@gmail.com>
2023-07-02 20:56:06 +0800

    refactor: the footer router link

--- src/layouts/theFooter.vue
+++ src/layouts/theFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <div class="container">
-      <a href="/" class="logo-font" @click="$router.push('/')">conduit</a>
+      <router-link class="logo-font" to="/">conduit</router-link>
       <span class="attribution">
         An interactive learning project from
         <a href="https://thinkster.io" target="_blank">Thinkster</a>. Code &amp; design licensed
