30328536d5823c883971b86f3de62186654db29b
Oliveria <frankiewb9527@gmail.com>
2023-07-07 10:14:22 +0800

    chore: add unplugin-vue-components/

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import Vue from '@vitejs/plugin-vue'
 import Layouts from 'vite-plugin-vue-layouts'
 import VueRouter from 'unplugin-vue-router/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { VueRouterAutoImports, getPascalCaseRouteName } from 'unplugin-vue-router'
 
 export default defineConfig({
@@ -16,6 +17,9 @@ export default defineConfig({
     Vue(),
     Layouts({
       defaultLayout: 'index'
+    }),
+    Components({
+      dts: true
     }),
     AutoImport({
       dts: true,
