2c2638dce32befb4eedf36256f2601edf34e79b0
Oliveria <frankiewb9527@gmail.com>
2023-07-08 23:53:08 +0800

    feat: add tags

---
+++ src/views/index/components/TagList.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { GetTags } from '@/api'
+
+const tags = ref<string[]>([])
+const getTags = async (): Promise<void> => {
+  const res = await GetTags()
+  tags.value = res.tags
+}
+
+onMounted(() => {
+  getTags()
+})
+</script>
+
+<template>
+  <div class="tag-list">
+    <router-link
+      to=""
+      :key="tag"
+      v-for="tag in tags"
+      class="tag-pill tag-default"
+      @click="$emit('change', tag)"
+    >
+      {{ tag }}
+    </router-link>
+  </div>
+</template>
