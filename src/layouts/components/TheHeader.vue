bda69a5ee1d057ce9bd6cd272768ae36fefcca08
Oliveria <frankiewb9527@gmail.com>
2023-07-09 20:26:42 +0800

    feat: add the header

--- src/layouts/components/TheHeader.vue
+++ src/layouts/components/TheHeader.vue
@@ -1,21 +1,41 @@
 <script lang="ts" setup>
-interface NavList {
-  label: string
-  path: string
-}
+import { useUserStore } from '@/stores/useUserStore'
 
-const navList = reactive<NavList[]>([
+const store = useUserStore()
+const { isLoggedIn, userInfo } = storeToRefs(store)
+const navList = computed(() => [
   {
     label: 'Home',
-    path: '/'
+    path: '/',
+    auth: true
   },
   {
     label: 'Sign in',
-    path: '/login'
+    path: '/login',
+    auth: !isLoggedIn.value
   },
   {
     label: 'Sign up',
-    path: '/register'
+    path: '/register',
+    auth: !isLoggedIn.value
+  },
+  {
+    label: 'New Article',
+    path: '/article/create',
+    auth: isLoggedIn.value,
+    icon: 'ion-compose'
+  },
+  {
+    label: 'Settings',
+    path: '/settings',
+    auth: isLoggedIn.value,
+    icon: 'ion-gear-a'
+  },
+  {
+    label: userInfo.value?.username,
+    path: '/profile',
+    auth: isLoggedIn.value,
+    pic: userInfo.value?.image
   }
 ])
 </script>
@@ -25,14 +45,15 @@ const navList = reactive<NavList[]>([
     <div class="container">
       <router-link class="navbar-brand" to="/">conduit</router-link>
       <ul class="nav navbar-nav pull-xs-right">
-        <li class="nav-item" :key="index" v-for="({ label, path }, index) in navList">
-          <router-link
-            :to="path"
-            :class="['nav-link', { active: $router.currentRoute.value.fullPath === path }]"
-          >
-            {{ label }}
-          </router-link>
-        </li>
+        <template :key="label" v-for="{ label, path, icon, pic, auth } in navList">
+          <li class="nav-item" v-if="auth">
+            <router-link :to="path" class="nav-link" active-class="active">
+              <template v-if="icon"><i :class="icon"></i>&nbsp;</template>
+              <template v-if="pic"><img :src="pic" class="user-pic" /></template>
+              {{ label }}
+            </router-link>
+          </li>
+        </template>
       </ul>
     </div>
   </nav>
