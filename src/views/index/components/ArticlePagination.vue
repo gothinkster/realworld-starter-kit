6037d924f7a38239119579d769e4096422334b04
Oliveria <frankiewb9527@gmail.com>
2023-07-10 13:40:19 +0800

    feat: add article pagination

---
+++ src/views/index/components/ArticlePagination.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const props = defineProps({
+  count: { type: Number, required: true },
+  current: { type: Number, required: true, default: 1 }
+})
+const pageCount = computed(() => Math.ceil(props.count / 10))
+</script>
+
+<template>
+  <ul class="pagination">
+    <li
+      :key="item"
+      v-for="item in pageCount"
+      :class="['page-item', { active: item === props.current }]"
+    >
+      <router-link to="" class="page-link" @click="$emit('currentChange', item)">
+        {{ item }}
+      </router-link>
+    </li>
+  </ul>
+</template>
