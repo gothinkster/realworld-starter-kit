3e9f18a7309ffdfb335e13061416f39b79131ca5
Niyaco <frankiewb9527@gmail.com>
2023-07-13 21:35:14 +0800

    refactor: update tag-list

--- src/views/index/components/TagList.vue
+++ src/views/index/components/TagList.vue
@@ -1,28 +1,20 @@
 <script lang="ts" setup>
 import api from '@/api'
+import { useAsyncState } from '@vueuse/core'
 
-const isError = ref(false)
-const tags = ref<string[]>([])
-const getTags = async (): Promise<void> => {
-  try {
-    const res = await api.getTags()
-
-    tags.value = res.tags
-  } catch {
-    isError.value = true
-  }
-}
-
-await getTags()
+const { isLoading, state, error } = useAsyncState(
+  api.getTags().then((res) => res.tags),
+  []
+)
 </script>
-
 <template>
-  <div v-if="isError">tags is error</div>
+  <div v-if="error">Tags is error</div>
+  <div v-else-if="isLoading">Loading tags...</div>
   <div v-else class="tag-list">
     <router-link
       to=""
       :key="tag"
-      v-for="tag in tags"
+      v-for="tag in state"
       class="tag-pill tag-default"
       @click="$emit('change', tag)"
     >
