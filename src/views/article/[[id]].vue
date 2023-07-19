2cc7e2c62b66b9ae70e319bfb047bc055465c103
Niyaco <frankiewb9527@gmail.com>
2023-07-19 16:19:00 +0800

    feat: add article and comment-list

---
+++ src/views/article/[[id]].vue
@@ -0,0 +1,106 @@
+<script lang="ts" setup>
+import api from '@/api'
+import type { Article } from '@/types'
+import { formattedDate } from '@/utils'
+import { useUserStore } from '@/stores/useUserStore'
+import commentList from './components/CommentList.vue'
+import type { RouteLocationNormalizedLoaded } from 'vue-router'
+
+const route = useRoute() as RouteLocationNormalizedLoaded
+const store = useUserStore()
+const article = ref<Article>({
+  title: '',
+  author: {
+    username: '',
+    image: '',
+    bio: '',
+    following: false
+  },
+  createdAt: '',
+  slug: '',
+  description: '',
+  body: '',
+  tagList: [],
+  updatedAt: '',
+  favorited: false,
+  favoritesCount: 0
+})
+const getActive = async () => {
+  const res = await api.getArticle(route.params.id as string)
+
+  article.value = res.article
+}
+
+const changeFacorited = (value: Article) => {
+  article.value = value
+}
+const changeFollow = (value: boolean) => {
+  article.value.author.following = value
+}
+
+onMounted(() => {
+  getActive()
+})
+</script>
+<template>
+  <div class="article-page">
+    <div class="banner">
+      <div class="container">
+        <h1>{{ article.title }}</h1>
+        <div class="article-meta">
+          <router-link to="">
+            <img :src="article.author.image" />
+          </router-link>
+          <div class="info">
+            <router-link to="" class="author">{{ article.author.username }}</router-link>
+            <span class="date">{{ formattedDate(article.createdAt) }}</span>
+          </div>
+          <follow-button :article="article" v-if="article.title" @change="changeFollow" />
+          &nbsp;&nbsp;
+          <favorite-button :article="article" v-if="article.title" @change="changeFacorited" />
+        </div>
+      </div>
+    </div>
+
+    <div class="container page">
+      <div class="row article-content">
+        <div class="col-xs-12">
+          <p>{{ article.description }}</p>
+          <p v-html="article.body"></p>
+          <tag-list :options="article.tagList" />
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="article-actions">
+        <div class="article-meta">
+          <router-link to="">
+            <img :src="article.author.image" />
+          </router-link>
+          <div class="info">
+            <router-link to="" class="author">{{ article.author.username }}</router-link>
+            <span class="date">{{ formattedDate(article.createdAt) }}</span>
+          </div>
+
+          <follow-button :article="article" v-if="article.title" @change="changeFollow" />
+          &nbsp;
+          <favorite-button :article="article" v-if="article.title" @change="changeFacorited" />
+        </div>
+      </div>
+
+      <div class="row" v-if="!store.isLoggedIn">
+        <div class="col-xs-12 col-md-8 offset-md-2">
+          <p>
+            <router-link to="/login">Sign in</router-link>
+            &nbsp; or &nbsp;
+            <router-link to="/register">Sign up</router-link>
+            &nbsp; to add comments on this article.
+          </p>
+        </div>
+      </div>
+
+      <comment-list v-else :article="article" /> 
+    </div>
+  </div>
+</template>
