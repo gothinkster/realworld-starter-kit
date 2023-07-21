67ab3750433f8afbac2d009f846fae6ea16cb989
Niyaco <frankiewb9527@gmail.com>
2023-07-21 11:45:28 +0800

    fix: Replace cookie with localStore

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -1,19 +1,18 @@
 import api from '@/api'
 import type { UserInfo, User } from '@/types'
-import { useCookies } from '@vueuse/integrations/useCookies'
 
 export const useUserStore = defineStore('user', () => {
-  const { set } = useCookies()
   const userInfo = ref<UserInfo | null>(null)
   const isLoggedIn = computed(() => userInfo.value !== null)
   const auth = async (type: string, formStore: { user: User }) => {
     const { user } = type === 'register' ? await api.createUser(formStore) : await api.login(formStore)
     userInfo.value = user
-    set('jwt-token', user.token)
+    localStorage.setItem('jwt-token', user.token as string)
   }
   const getUserInfo = async () => {
     const { user } = await api.getUserInfo()
     userInfo.value = user
+    localStorage.setItem('jwt-token', user.token as string)
   }
 
   return { userInfo, isLoggedIn, auth, getUserInfo }
