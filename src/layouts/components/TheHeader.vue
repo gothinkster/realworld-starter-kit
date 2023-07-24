7f73f825e85f29eb8435af0bc08b347cf69e476a
Niyaco <frankiewb9527@gmail.com>
2023-07-24 16:02:33 +0800

    style: rename variable name

--- src/layouts/components/TheHeader.vue
+++ src/layouts/components/TheHeader.vue
@@ -7,34 +7,34 @@ const navList = computed(() => [
   {
     label: 'Home',
     path: '/',
-    auth: true
+    show: true
   },
   {
     label: 'Sign in',
     path: '/login',
-    auth: !isLoggedIn.value
+    show: !isLoggedIn.value
   },
   {
     label: 'Sign up',
     path: '/register',
-    auth: !isLoggedIn.value
+    show: !isLoggedIn.value
   },
   {
     label: 'New Article',
     path: '/article/create',
-    auth: isLoggedIn.value,
+    show: isLoggedIn.value,
     icon: 'ion-compose'
   },
   {
     label: 'Settings',
     path: '/settings',
-    auth: isLoggedIn.value,
+    show: isLoggedIn.value,
     icon: 'ion-gear-a'
   },
   {
     label: userInfo.value?.username,
     path: `/profile/${store.userInfo?.username}`,
-    auth: isLoggedIn.value,
+    show: isLoggedIn.value,
     pic: userInfo.value?.image
   }
 ])
@@ -45,8 +45,8 @@ const navList = computed(() => [
     <div class="container">
       <router-link class="navbar-brand" to="/">conduit</router-link>
       <ul class="nav navbar-nav pull-xs-right">
-        <template :key="label" v-for="{ label, path, icon, pic, auth } in navList">
-          <li class="nav-item" v-if="auth">
+        <template :key="label" v-for="{ label, path, icon, pic, show } in navList">
+          <li class="nav-item" v-if="show">
             <router-link :to="path" class="nav-link" active-class="active">
               <template v-if="icon"><i :class="icon"></i>&nbsp;</template>
               <template v-if="pic"><img :src="pic" class="user-pic" /></template>
