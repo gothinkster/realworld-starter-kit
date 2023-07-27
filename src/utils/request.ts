8d85fb5114e4ed358292706f4152cf6fd3ae227f
Niyaco <frankiewb9527@gmail.com>
2023-07-27 10:39:38 +0800

    refactor: update request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,29 +1,23 @@
-const service = axios.create({ timeout: 10000 })
+import axios from 'axios'
 
-service.interceptors.request.use(
+const instance = axios.create({ timeout: 10000 })
+
+instance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('jwt-token')
 
-    if (token) config.headers['Authorization'] = `Bearer ${token}`
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
 
     return config
   },
-  (error) => {
-    return Promise.reject(error)
-  }
+  error => Promise.reject(error)
 )
 
-service.interceptors.response.use(
-  (response) => {
-    return response.data
-  },
-  (error) => {
-    if (error.response) {
-      return Promise.reject(error.response.data)
-    } else {
-      return Promise.reject('error')
-    }
-  }
+instance.interceptors.response.use(
+  ({ data }) => data,
+  ({ message, response }) => Promise.reject(response ? response.data : message)
 )
 
-export default service
+export default instance
