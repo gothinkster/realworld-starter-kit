29c9f40251af8fa4dbd467c83975b53c929bbf49
Niyaco <frankiewb9527@gmail.com>
2023-07-27 15:59:56 +0800

    chore: add env variable

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const instance = axios.create({ timeout: 10000 })
+const instance = axios.create({
+  timeout: 10000,
+  baseURL: import.meta.env.VITE_BASE_URL
+})
 
 instance.interceptors.request.use(
   (config) => {
