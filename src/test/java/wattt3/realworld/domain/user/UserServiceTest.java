587980cdc3d1d532275ea6eec70fe642db1404c7
wattt3 <chleodnr3@gmail.com>
2023-08-29 14:47:15 +0900

    6. create User Domain / create UserRegisterRequest toDomain()

--- src/test/java/wattt3/realworld/domain/user/UserServiceTest.java
+++ src/test/java/wattt3/realworld/domain/user/UserServiceTest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 public class UserServiceTest {
 
@@ -22,6 +23,7 @@ public class UserServiceTest {
             "password");
 
         userService.register(request);
+
     }
 
     public record UserRegisterRequest(
@@ -29,15 +31,42 @@ public class UserServiceTest {
         String email,
         @NotBlank(message = "유저명은 필수입니다.")
         String username,
-        @NotBlank(message = "비밀번호는 필수입니다.")
+        @NotBlank(message = "패스워드는 필수입니다.")
         String password) {
 
+        public User toDomain() {
+            return new User(email, username, password, null, null);
+        }
+    }
+
+    public static class User {
+
+        private final String email;
+        private final String username;
+        private final String password;
+        private final String bio;
+        private final String image;
+
+        public User(String email, String username, String password, String bio, String image) {
+            validateUser(email, username, password);
+            this.email = email;
+            this.username = username;
+            this.password = password;
+            this.bio = bio;
+            this.image = image;
+        }
+
+        private void validateUser(String email, String username, String password) {
+            Assert.hasText(email, "이메일은 필수입니다.");
+            Assert.hasText(username, "유저명은 필수입니다.");
+            Assert.hasText(password, "패스워드는 필수입니다.");
+        }
     }
 
     public class UserService {
 
         public void register(UserRegisterRequest request) {
+            User user = request.toDomain();
         }
     }
-
 }
