7719b1649c348d65b3c32275893cbf8d23134003
wattt3 <chleodnr3@gmail.com>
2023-08-29 15:03:58 +0900

    7. create UserRepository / implement UserService

--- src/test/java/wattt3/realworld/domain/user/UserServiceTest.java
+++ src/test/java/wattt3/realworld/domain/user/UserServiceTest.java
@@ -1,7 +1,13 @@
 package wattt3.realworld.domain.user;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,10 +16,12 @@ import org.springframework.util.Assert;
 public class UserServiceTest {
 
     private UserService userService;
+    private UserRepository userRepository;
 
     @BeforeEach
     void setUp() {
-        userService = new UserService();
+        userRepository = new UserRepository();
+        userService = new UserService(userRepository);
     }
 
     @Test
@@ -23,7 +31,7 @@ public class UserServiceTest {
             "password");
 
         userService.register(request);
-
+        assertThat(userRepository.findAll().size()).isEqualTo(1);
     }
 
     public record UserRegisterRequest(
@@ -46,6 +54,7 @@ public class UserServiceTest {
         private final String password;
         private final String bio;
         private final String image;
+        private Long id;
 
         public User(String email, String username, String password, String bio, String image) {
             validateUser(email, username, password);
@@ -61,12 +70,44 @@ public class UserServiceTest {
             Assert.hasText(username, "유저명은 필수입니다.");
             Assert.hasText(password, "패스워드는 필수입니다.");
         }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void assignId(Long id) {
+            this.id = id;
+        }
     }
 
     public class UserService {
 
+        private final UserRepository userRepository;
+
+        public UserService(UserRepository userRepository) {
+            this.userRepository = userRepository;
+        }
+
         public void register(UserRegisterRequest request) {
             User user = request.toDomain();
+            userRepository.save(user);
         }
+    }
+
+    private class UserRepository {
+
+        private final Map<Long, User> users = new HashMap<>();
+        private Long sequence = 1L;
+
+        public void save(final User user) {
+            user.assignId(sequence);
+            sequence++;
+            users.put(user.getId(), user);
+        }
+
+        public List<User> findAll() {
+            return new ArrayList<>(users.values());
+        }
+
     }
 }
