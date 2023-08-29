0101adebcf75da189ca1dab51548ea1caab6781b
wattt3 <chleodnr3@gmail.com>
2023-08-29 13:50:28 +0900

    2. create test case registerUser

--- src/test/java/wattt3/realworld/domain/user/UserServiceTest.java
+++ src/test/java/wattt3/realworld/domain/user/UserServiceTest.java
@@ -1,5 +1,13 @@
 package wattt3.realworld.domain.user;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserServiceTest {
 
+    @Test
+    @DisplayName("유저 등록(회원가입)")
+    void registerUser() {
+
+    }
 }
