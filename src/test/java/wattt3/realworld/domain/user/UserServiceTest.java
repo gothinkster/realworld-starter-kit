24290dbdc001907e53ca119392b7f6aa94b43fe6
wattt3 <chleodnr3@gmail.com>
2023-08-29 14:26:10 +0900

    4. create UserRegisterRequest

--- src/test/java/wattt3/realworld/domain/user/UserServiceTest.java
+++ src/test/java/wattt3/realworld/domain/user/UserServiceTest.java
@@ -1,5 +1,7 @@
 package wattt3.realworld.domain.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +10,17 @@ public class UserServiceTest {
     @Test
     @DisplayName("유저 등록(회원가입)")
     void registerUser() {
+        UserRegisterRequest request = new UserRegisterRequest("email@email.com", "username",
+            "password");
+    }
+
+    public record UserRegisterRequest(
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        String email,
+        @NotBlank(message = "유저명은 필수입니다.")
+        String username,
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        String password) {
 
     }
 }
