88d0a18ef0a9a42f2af1faffc55bb57199764d6a
wattt3 <chleodnr3@gmail.com>
2023-08-30 11:36:19 +0900

    14. create ApiTest

---
+++ src/test/java/wattt3/realworld/user/ApiTest.java
@@ -0,0 +1,22 @@
+package wattt3.realworld.user;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApiTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+        }
+    }
+
+}
--- src/test/java/wattt3/realworld/user/domain/UserApiTest.java
+++ src/test/java/wattt3/realworld/user/domain/UserApiTest.java
@@ -4,33 +4,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.transaction.annotation.Transactional;
+import wattt3.realworld.user.ApiTest;
 import wattt3.realworld.user.application.request.RegisterUserRequest;
 import wattt3.realworld.user.infra.UserRepository;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@Transactional
-public class UserServiceTest {
+public class UserApiTest extends ApiTest {
 
-    @LocalServerPort
-    private int port;
     @Autowired
     private UserRepository userRepository;
-
-    @BeforeEach
-    void setUp() {
-        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
-            RestAssured.port = port;
-        }
-    }
 
     @Test
     @DisplayName("유저 등록(회원가입)")
