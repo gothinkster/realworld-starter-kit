7f74320cee68da36a31da9054a2beccaef8b8971
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-09-08 00:46:45 -0300

    conf:first alembic migration for users tables

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,5 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.models import Base
 
 from app.app import app
 
@@ -7,3 +11,12 @@ from app.app import app
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    yield Session()
+    Base.metadata.drop_all(engine)
---
+++ app/tests/test_db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import select
+from app.models import User
+
+
+def test_create_user(session):
+    new_user = User(username='alice', password='secret', email='test@test')
+    session.add(new_user)
+    session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'alice'))
+
+    assert user.username == 'alice'
