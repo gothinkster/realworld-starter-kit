c982eded00cb1576d46fcf1e3b6677dce7f75e7c
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-09-18 02:05:08 -0300

    feat: first migration, adding user tables

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,9 +1,21 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from api.app import app
+from api.models import Base
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    yield Session()
+    Base.metadata.drop_all(engine)
