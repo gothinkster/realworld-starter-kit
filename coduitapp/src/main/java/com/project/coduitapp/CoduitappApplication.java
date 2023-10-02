286712368a8ddc2f8d4eef8f4d42a71d91c60e0d
Deep <105926444+Dpppatel@users.noreply.github.com>
2023-10-02 14:43:50 -0500

    setup

---
+++ coduitapp/src/main/java/com/project/coduitapp/CoduitappApplication.java
@@ -0,0 +1,13 @@
+package com.project.coduitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoduitappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoduitappApplication.class, args);
+	}
+
+}
---
+++ coduitapp/src/test/java/com/project/coduitapp/CoduitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coduitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoduitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
