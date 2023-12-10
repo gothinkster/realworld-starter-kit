a8ba54c5340d8044192031761b6d07cbb08363da
HACHI3232 <harada.chisato526@gmail.com>
2023-12-11 00:09:43 +0900

    ルーティングの変更（article）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'articles/new'
-  get 'articles/edit'
-  get 'articles/show'
+  resources :articles, except: [:show] 
+  get '/article/:id', to: 'articles#show', as: 'show_article'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
