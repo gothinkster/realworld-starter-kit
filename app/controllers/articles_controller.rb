8c8a19731a91500e8420e66e288e94e67a386897
HACHI3232 <harada.chisato526@gmail.com>
2023-12-11 00:53:25 +0900

    記事の投稿ができるように変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,26 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description, :body, :created_at, :update_at)
   end
 end
