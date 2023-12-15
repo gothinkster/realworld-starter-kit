c433f1fa8f5266112e9cc434a896f7413c29cf52
HACHI3232 <harada.chisato526@gmail.com>
2023-12-16 02:36:02 +0900

    記事取得時に関連ユーザーを事前読み込みするよう変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.all.order("created_at DESC")
+    @articles = Article.includes(:user).order("created_at DESC")
   end
 end
