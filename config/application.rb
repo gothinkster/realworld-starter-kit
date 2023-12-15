fd27d58eb5bd5e7819f9051869ae70962364ac94
HACHI3232 <harada.chisato526@gmail.com>
2023-12-15 18:56:00 +0900

    ⾃動的にファクトリを作成するように Rails を設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module RealworldStarterKit
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
