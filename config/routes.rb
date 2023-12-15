81648522ed9ddf8a12e2e41597b5f034babdc999
HACHI3232 <harada.chisato526@gmail.com>
2023-12-15 23:43:53 +0900

    userのルーティングを追加、リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :articles, except: [:show] 
-  get '/article/:id', to: 'articles#show', as: 'show_article'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
   root 'home#index'
+
+  resources :articles, except: [:show]
+  get '/article/:id', to: 'articles#show', as: 'show_article'
+  resources :users, only: :show
+  get "up" => "rails/health#show", as: :rails_health_check
 end
