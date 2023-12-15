8691fd2752ae58a6b9a16290f8aa8b70ecd0e8e3
HACHI3232 <harada.chisato526@gmail.com>
2023-12-15 18:52:45 +0900

    新規ユーザー登録時のemailに対するバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true, length: { in: 3..6, message: 'must be between 3 and 6 characters' }
+  validates :username, presence: true, length: { in: 3..15, message: 'must be between 3 and 15 characters' }
+  validates :email, presence: true, uniqueness: true
 end
